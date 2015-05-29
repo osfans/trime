0f487c502f9a3cb830ab40f0beee4cc61f26179a
osfans <waxaca@163.com>
2015-05-29 10:38:27 +0800

    Add boost 1.58 mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -19,3 +19,4 @@ include jni/yaml-cpp.mk #0.5.2
 include jni/snappy.mk #1.12
 include jni/leveldb.mk #1.18
 include jni/marisa-trie.mk #0.2.4
+include jni/boost.mk #1.58
---
+++ jni/boost.mk
@@ -0,0 +1,45 @@
+LOCAL_PATH := $(ROOT_PATH)/boost
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := boost #1.58
+
+LOCAL_SRC_FILES += \
+  libs/filesystem/src/operations.cpp \
+  libs/filesystem/src/utf8_codecvt_facet.cpp \
+  libs/filesystem/src/codecvt_error_category.cpp \
+  libs/filesystem/src/portability.cpp \
+  libs/filesystem/src/unique_path.cpp \
+  libs/filesystem/src/path_traits.cpp \
+  libs/filesystem/src/windows_file_codecvt.cpp \
+  libs/filesystem/src/path.cpp \
+  \
+  libs/regex/src/c_regex_traits.cpp \
+  libs/regex/src/winstances.cpp \
+  libs/regex/src/icu.cpp \
+  libs/regex/src/cregex.cpp \
+  libs/regex/src/regex_raw_buffer.cpp \
+  libs/regex/src/fileiter.cpp \
+  libs/regex/src/wide_posix_api.cpp \
+  libs/regex/src/usinstances.cpp \
+  libs/regex/src/static_mutex.cpp \
+  libs/regex/src/cpp_regex_traits.cpp \
+  libs/regex/src/w32_regex_traits.cpp \
+  libs/regex/src/wc_regex_traits.cpp \
+  libs/regex/src/regex_debug.cpp \
+  libs/regex/src/posix_api.cpp \
+  libs/regex/src/instances.cpp \
+  libs/regex/src/regex_traits_defaults.cpp \
+  libs/regex/src/regex.cpp \
+  \
+  libs/signals/src/connection.cpp \
+  libs/signals/src/named_slot_map.cpp \
+  libs/signals/src/trackable.cpp \
+  libs/signals/src/slot.cpp \
+  libs/signals/src/signal_base.cpp \
+  libs/system/src/error_code.cpp \
+
+LOCAL_LDLIBS := -latomic
+# 如果要把boost集成到动态库里，-fPIC是必须的，不然会有链接错误。原因请自行Google
+#LOCAL_CFLAGS += -fPIC -frtti -fexceptions
+include $(BUILD_SHARED_LIBRARY)
