80af413f5000b3e25ed9dd69b3027c6478745572
osfans <waxaca@163.com>
2015-08-13 17:15:19 +0800

    jni: rename to rime_jni.cc

--- jni/librime_jni.mk
+++ jni/librime_jni.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(ROOT_PATH)/librime_jni
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rime_jni
-LOCAL_SRC_FILES := rime_jni.cpp
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_SRC_FILES := rime_jni.cc
 LOCAL_SHARED_LIBRARIES := rime
 LOCAL_CFLAGS := -DCLASSNAME=\"com/osfans/trime/Rime\" -DOPENCC_VERSION="\"$(shell git --git-dir $(ROOT_PATH)/OpenCC/.git describe --tags)\"" -DLIBRIME_VERSION="\"$(shell git --git-dir $(ROOT_PATH)/librime/.git describe --tags)\""
 LOCAL_LDLIBS := -llog -latomic
