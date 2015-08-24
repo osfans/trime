86f0714ac35b1072ac136b06642ea7d42a4e4ee7
osfans <waxaca@163.com>
2015-08-24 09:25:30 +0800

    jni: update boost & snappy

--- jni/snappy.mk
+++ jni/snappy.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(ROOT_PATH)/snappy
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := snappy #1.1.2
+LOCAL_MODULE := snappy #1.1.3
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := snappy.cc snappy-sinksource.cc
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
