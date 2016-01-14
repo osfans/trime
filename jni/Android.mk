ed3bf98a3a39c04b2d7ef0acd03e725d249e7c95
osfans <waxaca@163.com>
2016-01-14 09:41:29 +0800

    jni: update yaml-cpp to 0.5.3

--- jni/Android.mk
+++ jni/Android.mk
@@ -15,12 +15,12 @@
 
 ROOT_PATH := $(call my-dir)
 include jni/opencc.mk #1.0.3
-include jni/yaml-cpp.mk #0.5.2
+include jni/yaml-cpp.mk #0.5.3
 include jni/snappy.mk #1.1.3
 include jni/leveldb.mk #1.18
 include jni/marisa.mk #0.2.4
 include jni/boost.mk #1.59
-include jni/miniglog.mk
+include jni/libiconv.mk #1.14
 include jni/librime.mk #1.2.9
 include jni/librime_jni.mk
-include jni/libiconv.mk #1.14
+include jni/miniglog.mk
