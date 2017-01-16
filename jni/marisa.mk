aa3805feb47bc058edffcdd44e84638b78552a15
osfans <waxaca@163.com>
2017-01-16 11:59:55 +0800

    refactor(Makefile): use wildcard

--- jni/marisa.mk
+++ jni/marisa.mk
@@ -5,15 +5,8 @@ LOCAL_MODULE := marisa
 LOCAL_CPP_EXTENSION := .cc
 
 LOCAL_SRC_FILES := \
-	lib/marisa/agent.cc \
-	lib/marisa/keyset.cc \
-	lib/marisa/trie.cc \
-	lib/marisa/grimoire/io/mapper.cc \
-	lib/marisa/grimoire/io/reader.cc \
-	lib/marisa/grimoire/io/writer.cc \
-	lib/marisa/grimoire/trie/louds-trie.cc \
-	lib/marisa/grimoire/trie/tail.cc \
-	lib/marisa/grimoire/vector/bit-vector.cc
+  $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/lib/marisa/*.cc)) \
+  $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/lib/marisa/grimoire/**/**.cc))
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/lib #$(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
--- jni/yaml-cpp.mk
+++ jni/yaml-cpp.mk
@@ -7,33 +7,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
 LOCAL_SRC_FILES := \
-    src/binary.cpp \
-    src/convert.cpp \
-    src/directives.cpp \
-    src/emit.cpp \
-    src/emitfromevents.cpp \
-    src/emitter.cpp \
-    src/emitterstate.cpp \
-    src/emitterutils.cpp \
-    src/exp.cpp \
-    src/memory.cpp \
-    src/nodebuilder.cpp \
-    src/node.cpp \
-    src/node_data.cpp \
-    src/nodeevents.cpp \
-    src/null.cpp \
-    src/ostream_wrapper.cpp \
-    src/parse.cpp \
-    src/parser.cpp \
-    src/regex_yaml.cpp \
-    src/scanner.cpp \
-    src/scanscalar.cpp \
-    src/scantag.cpp \
-    src/scantoken.cpp \
-    src/simplekey.cpp \
-    src/singledocparser.cpp \
-    src/stream.cpp \
-    src/tag.cpp
+  $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/src/*.cpp))
 
 #LOCAL_LDLIBS := -latomic
 LOCAL_STATIC_LIBRARIES := boost
