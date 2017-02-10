fa653a90dd697afe2e3b912e3bd2727b3912317c
osfans <waxaca@163.com>
2017-02-10 12:28:51 +0800

    chore(jni): add abi in rime version

--- jni/librime_jni.mk
+++ jni/librime_jni.mk
@@ -6,6 +6,6 @@ LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/*.cc))
 LOCAL_SHARED_LIBRARIES := rime
 LOCAL_STATIC_LIBRARIES := opencc
-LOCAL_CFLAGS := -DOPENCC_VERSION="\"$(shell git --git-dir $(ROOT_PATH)/OpenCC/.git describe --tags)\"" -DLIBRIME_VERSION="\"$(shell git --git-dir $(ROOT_PATH)/librime/.git describe --tags)\""
+LOCAL_CFLAGS := -DOPENCC_VERSION="\"$(shell git --git-dir $(ROOT_PATH)/OpenCC/.git describe --tags)\"" -DLIBRIME_VERSION="\"$(shell git --git-dir $(ROOT_PATH)/librime/.git describe --tags)-${TARGET_ARCH_ABI}\""
 LOCAL_LDLIBS := -llog -latomic
 include $(BUILD_SHARED_LIBRARY)
