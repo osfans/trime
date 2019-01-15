4907bae02789d59ceaca60feeecf4999fbd9e689
osfans <waxaca@163.com>
2019-01-15 10:26:00 +0800

    chore: upgrade to gradle 5.1.1, AS 3.3.0 and boost 1.69

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -19,7 +19,7 @@ include jni/yaml-cpp.mk #0.6.2
 include jni/snappy.mk #1.1.7
 include jni/leveldb.mk #1.20
 include jni/marisa.mk #0.2.5
-include jni/boost.mk #1.66.0
+include jni/boost.mk #1.69.0
 include jni/libiconv.mk #1.15
 include jni/librime.mk #1.3.2
 include jni/librime_jni.mk
