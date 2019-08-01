397fbd9815d26a9204f7a692c4ff1bdd83d120ec
osfans <waxaca@163.com>
2019-08-01 17:45:56 +0800

    chore: remove ndk-build makefiles

--- app/src/main/jni/miniglog.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(ROOT_PATH)/miniglog
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := miniglog
-LOCAL_CPP_EXTENSION := .cc
-LOCAL_SRC_FILES := miniglog/logging.cc
-LOCAL_EXPORT_LDLIBS := -llog
-include $(BUILD_STATIC_LIBRARY)
--- app/src/main/jni/snappy.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(ROOT_PATH)/snappy
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := snappy
-LOCAL_CPP_EXTENSION := .cc
-LOCAL_SRC_FILES := snappy.cc snappy-sinksource.cc
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-
-include $(BUILD_STATIC_LIBRARY)
