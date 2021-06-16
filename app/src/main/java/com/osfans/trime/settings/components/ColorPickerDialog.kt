c0b767cfeb4fec4d16d113a4b4b59566b6b13217
WhiredPlanck <whiredplanck@outlook.com>
2021-06-17 00:43:02 +0800

    Init theme class and complete color class and reset class

--- app/src/main/java/com/osfans/trime/settings/components/ColorPickerDialog.kt
+++ app/src/main/java/com/osfans/trime/settings/components/ColorPickerDialog.kt
@@ -1,4 +1,50 @@
 package com.osfans.trime.settings.components
 
-class ColorPickerDialog {
+import android.app.AlertDialog
+import android.content.Context
+import com.osfans.trime.Config
+import com.osfans.trime.R
+import com.osfans.trime.Trime
+
+/** 顯示配色方案列表
+ *  Show Color Scheme List
+ * **/
+class ColorPickerDialog(
+    context: Context
+) {
+    val config: Config = Config.get(context)
+    private var colorKeys: Array<String>
+    private var checkedColor: Int = 0
+    var pickerDialog: AlertDialog
+        private set // 设定方法不向外开放
+
+    init {
+        val colorScheme = config.colorScheme
+        colorKeys = config.colorKeys
+        colorKeys.sort()
+        val colorNames = config.getColorNames(colorKeys)
+        checkedColor = colorKeys.binarySearch(colorScheme)
+
+        pickerDialog = AlertDialog.Builder(context).apply {
+            setTitle(R.string.pref_colors)
+            setCancelable(true)
+            setNegativeButton(android.R.string.cancel, null)
+            setPositiveButton(android.R.string.ok) { _, _ ->
+                selectColor()
+            }
+            setSingleChoiceItems(
+                colorNames, checkedColor
+            ) {_, id -> checkedColor = id}
+        }.create()
+    }
+
+    private fun selectColor() {
+        if (checkedColor < 0 || checkedColor >= colorKeys.size) return
+        val colorKey = colorKeys[checkedColor]
+        config.setColor(colorKey)
+        Trime.getService()?.initKeyboard() // 立刻重初始化键盘生效
+    }
+
+    /** 调用该方法显示对话框 **/
+    fun show() = pickerDialog.show()
 }
