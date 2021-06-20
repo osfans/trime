42ab10775ef7edd9e707bec2c233b6fdd5c8f66a
WhiredPlanck <whiredplanck@outlook.com>
2021-06-20 13:06:51 +0800

    Complete methods

--- app/src/main/java/com/osfans/trime/settings/PrefMainActivity.kt
+++ app/src/main/java/com/osfans/trime/settings/PrefMainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.osfans.trime.R
 import com.osfans.trime.databinding.PrefActivityBinding
 
@@ -20,12 +21,43 @@ class PrefMainActivity: AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        if (savedInstanceState == null) {
+            loadFragment(PrefFragment())
+        } else {
+            title = savedInstanceState.getCharSequence(FRAGMENT_TAG)
+        }
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                setTitle(R.string.ime_name)
+            }
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
-    
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putCharSequence(FRAGMENT_TAG, title)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (supportFragmentManager.popBackStackImmediate()) {
+            return true
+        }
+        return super.onSupportNavigateUp()
+    }
+
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager
             .beginTransaction()
             .replace(binding.preference.id, fragment, FRAGMENT_TAG)
             .commit()
+    }
+
+    class PrefFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.prefs, rootKey)
+        }
     }
 }
