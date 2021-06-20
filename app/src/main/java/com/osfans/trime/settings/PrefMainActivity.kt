cc93fb52f9716641cf70886d6ccc4fd76be2a0ef
WhiredPlanck <whiredplanck@outlook.com>
2021-06-20 08:56:13 +0800

    Add toolbar support

--- app/src/main/java/com/osfans/trime/settings/PrefMainActivity.kt
+++ app/src/main/java/com/osfans/trime/settings/PrefMainActivity.kt
@@ -2,7 +2,12 @@ package com.osfans.trime.settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.osfans.trime.R
 import com.osfans.trime.databinding.PrefActivityBinding
+
+internal const val FRAGMENT_TAG = "FRAGMENT_TAG"
 
 class PrefMainActivity: AppCompatActivity() {
 
@@ -12,5 +17,15 @@ class PrefMainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = PrefActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+    
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.preference.id, fragment, FRAGMENT_TAG)
+            .commit()
     }
 }
