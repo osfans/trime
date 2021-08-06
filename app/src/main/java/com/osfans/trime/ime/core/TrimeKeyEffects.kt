509dd052311fbc1c4bfa062f649f201df023303c
WhiredPlanck <whiredplanck@outlook.com>
2021-08-06 23:40:32 +0800

    core: add a new effect manager

---
+++ app/src/main/java/com/osfans/trime/ime/core/TrimeKeyEffects.kt
@@ -0,0 +1,146 @@
+package com.osfans.trime.ime.core
+
+import android.content.Context
+import android.media.AudioManager
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.speech.tts.TextToSpeech
+import android.view.HapticFeedbackConstants
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import com.osfans.trime.databinding.InputRootBinding
+import java.util.*
+import kotlin.math.ln
+
+/**
+ * Manage the key press effects, such as vibration, sound, speaking and so on.
+ */
+class TrimeKeyEffects(
+    context: Context,
+    val prefs: Preferences = Preferences.defaultInstance()
+) {
+    private var inputRootBinding: InputRootBinding? = null
+
+    private var vibrator: Vibrator? = null
+    private var audioManager: AudioManager? = null
+    private var tts: TextToSpeech? = null
+
+    init {
+        try {
+            inputRootBinding = InputRootBinding.inflate(LayoutInflater.from(context))
+            vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as? Vibrator
+            audioManager = context.getSystemService(Context.AUDIO_SERVICE) as? AudioManager
+            tts = TextToSpeech(context) { }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Makes a key press vibration if the user has this feature enabled in the preferences.
+     */
+    fun keyPressVibrate() {
+        if (prefs.keyboard.vibrationEnabled) {
+            var vibrationDuration = prefs.keyboard.vibrationDuration.toLong()
+            var vibrationAmplitude = prefs.keyboard.vibrationAmplitude
+
+            val hapticsPerformed = if (vibrationDuration <0 && vibrationAmplitude < 0) {
+                inputRootBinding?.keyboard?.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+            } else {
+                false
+            }
+
+            if (hapticsPerformed == true) {
+                return
+            }
+
+            if (vibrationDuration == -1L) {
+                vibrationDuration = 36
+            }
+
+            if (vibrationAmplitude == -1 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrationAmplitude = VibrationEffect.DEFAULT_AMPLITUDE
+            } else if (vibrationAmplitude == -1) {
+                vibrationAmplitude = 36
+            }
+
+            if (vibrationAmplitude > 0) {
+                vibrationAmplitude = (vibrationAmplitude / 2.0).toInt().coerceAtLeast(1)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator?.vibrate(
+                    VibrationEffect.createOneShot(
+                        vibrationDuration, vibrationAmplitude
+                    )
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                vibrator?.vibrate(vibrationDuration)
+            }
+        }
+    }
+
+    /**
+     * Makes a key press sound if the user has this feature enabled in the preferences.
+     */
+    fun keyPressSound(keyCode: Int? = null) {
+        if (prefs.keyboard.soundEnabled) {
+            val soundVolume = prefs.keyboard.soundVolume
+            val effect = when (keyCode) {
+                KeyEvent.KEYCODE_SPACE -> AudioManager.FX_KEYPRESS_SPACEBAR
+                KeyEvent.KEYCODE_DEL -> AudioManager.FX_KEYPRESS_DELETE
+                KeyEvent.KEYCODE_ENTER -> AudioManager.FX_KEYPRESS_RETURN
+                else -> AudioManager.FX_KEYPRESS_STANDARD
+            }
+
+            if (soundVolume > 0) {
+                audioManager!!.playSoundEffect(effect,
+                    (1 - (ln((101.0 - soundVolume)) / ln(101.0))).toFloat()
+                )
+            }
+        }
+    }
+
+    /**
+     * Makes a key press speaking if the user has this feature enabled in the preferences.
+     */
+    fun keyPressSpeak(content: Any? = null) {
+        if (prefs.keyboard.isSpeakKey) contentSpeakInternal(content)
+    }
+
+    /**
+     * Makes a text commit speaking if the user has this feature enabled in the preferences.
+     */
+    fun textCommitSpeak(text: CharSequence? = null) {
+        if (prefs.keyboard.isSpeakCommit) contentSpeakInternal(text)
+    }
+
+    private inline fun <reified T> contentSpeakInternal(content: T) {
+        val text = when {
+            0 is T -> {
+                KeyEvent.keyCodeToString(content as Int)
+                    .replace("KEYCODE_", "")
+                    .replace("_", " ")
+                    .lowercase(Locale.getDefault())
+            }
+            "" is T -> content as String
+            else -> null
+        } ?: return
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "TrimeTTS")
+        } else {
+            @Suppress("DEPRECATION")
+            tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+        }
+    }
+
+    fun destroy() {
+        inputRootBinding = null
+        vibrator = null
+        audioManager = null
+        tts = null
+    }
+}
