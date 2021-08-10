22461d08e416dfa2aea8a12cfda474e6ddc604c4
WhiredPlanck <whiredplanck@outlook.com>
2021-08-10 16:02:22 +0800

    util: `AppVersionUtil.kt`: optimization
    also:
    - `Function.java`: supplement

--- app/src/main/java/com/osfans/trime/util/AppVersionUtils.kt
+++ app/src/main/java/com/osfans/trime/util/AppVersionUtils.kt
@@ -1,23 +1,20 @@
 package com.osfans.trime.util
 
-import android.content.Context
-import androidx.preference.PreferenceManager
+import com.blankj.utilcode.util.AppUtils
+import com.osfans.trime.ime.core.Preferences
 
 object AppVersionUtils {
-    fun getRawVersionName(context: Context): String {
-        return try {
-            context.packageManager.getPackageInfo(context.packageName, 0).versionName
-        } catch (e: Exception) {
-            "undefined"
-        }
-    }
-
-    fun isDifferentVersion(context: Context): Boolean {
-        val version = getRawVersionName(context)
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        val prefVersion = prefs.getString("version_name", "")
-        return !version.contentEquals(prefVersion).also {
-            prefs.edit().putString("version_name", version).apply()
+    /**
+     * Compare the application's version name
+     * to check if this version is a newer version.
+     *
+     * @param prefs the [Preferences] instance.
+     */
+    fun isDifferentVersion(prefs: Preferences): Boolean {
+        val currentVersionName = AppUtils.getAppVersionName()
+        val lastVersionName = prefs.general.lastVersionName
+        return !currentVersionName.contentEquals(lastVersionName).also {
+            prefs.general.lastVersionName = currentVersionName
         }
     }
 }
