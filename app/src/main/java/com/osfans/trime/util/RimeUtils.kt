b9e500b064462ed5888bb17dcf850cbd33fd07cc
WhiredPlanck <whiredplanck@outlook.com>
2021-09-08 00:12:56 +0800

    utils: `RimeUtils`: reorganize

--- app/src/main/java/com/osfans/trime/util/RimeUtils.kt
+++ app/src/main/java/com/osfans/trime/util/RimeUtils.kt
@@ -7,22 +7,28 @@ import com.osfans.trime.Rime
 import kotlin.system.exitProcess
 
 /**
- *  This object is a collection of common methods
- *  related to Rime JNI.
- */
+ * Wrapper of often-used native methods in [Rime].
+ **/
 object RimeUtils {
+    /** Check Rime backend health. **/
     fun check() {
         Rime.check(true)
         exitProcess(0) // Clear the memory
     }
 
+    /** Deploy means reset Rime instance. **/
     fun deploy(context: Context) {
         Rime.destroy()
         Rime.get(context, true)
-        ToastUtils.showLong(
-            R.string.deploy_finish
-        )
+        ToastUtils.showLong(R.string.deploy_finish)
     }
 
-    fun sync(context: Context) = Rime.syncUserData(context)
+    /** Sync the user data.
+     * @return `true` if successfully **/
+    fun sync(context: Context): Boolean {
+        return Rime.sync_user_data().also {
+            Rime.destroy()
+            Rime.get(context, true)
+        }
+    }
 }
