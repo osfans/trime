2253e084f647222c60983bfb707adba27109b366
iovxw <iovxw@outlook.com>
2021-11-18 00:54:15 +0800

    chore(CI): add JNI cache

---
+++ .github/workflows/cache-hash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JNI_FILES="app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
+
+hash=$(git submodule status)
+hash=$hash$(sha256sum $JNI_FILES)
+hash=$(echo $hash | sha256sum | cut -c-64)
+
+echo "::set-output name=hash::$hash"
+
