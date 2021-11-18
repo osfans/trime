8c673fb943f49cbfa259962bb0d0d05c7bb57a0a
iovxw <iovxw@outlook.com>
2021-11-18 18:28:06 +0800

    chore(CI): add app/build.gradle to the JNI cache hash

--- script/cache-hash.sh
+++ script/cache-hash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JNI_FILES="app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
+JNI_FILES="app/build.gradle app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
 
 hash=$(git submodule status)
 hash=$hash$(sha256sum $JNI_FILES)
