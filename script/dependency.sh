4c231a9ecc7f73acca26149c1e71f370eb1e4d2e
Bambooin <BambooinLinux@163.com>
2021-11-21 13:46:51 +0800

    chore: upgrade capnproto to 0.9.1
    1. switch to official upstream repo
    2. upgrade capnproto to 0.9.1
    3. install capnproto from source

--- script/dependency.sh
+++ script/dependency.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-sudo add-apt-repository 'deb http://deb.debian.org/debian experimental main'
-sudo apt update
-sudo apt-get -t experimental -y install capnproto=0.8.0-1
+# version on submodule must match
+# update summodule if you change this
+version=0.9.1
+echo "$(nproc) thread to build"
+curl -O https://capnproto.org/capnproto-c++-${version}.tar.gz
+tar zxf capnproto-c++-${version}.tar.gz
+cd capnproto-c++-${version}
+./configure
+make -j$(nproc)
+sudo make install
