f87ac74ae6ef7521d21ea0ce64a83b0dfe9e76c8
Bambooin <ooverflow@outlook.com>
2021-12-02 20:11:03 +0800

    feat: enable manually trigger jni build with trick

--- script/cache-hash.sh
+++ script/cache-hash.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-JNI_FILES="app/build.gradle app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
+JNI_FILES="magic.txt app/build.gradle app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
 
 hash=$(git submodule status)
 hash=$hash$(sha256sum $JNI_FILES)
