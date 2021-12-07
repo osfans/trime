4cb5505053283ef4209937f29099842055111e3f
Bambooin <ooverflow@outlook.com>
2021-12-07 17:48:24 +0800

    fix(ci): extract essential elements for cache hit
    Only some elements of `app/buid.gradle` is jni relative, so extract it
    
    The commit of this should hit jni cache, but it's missing:
    https://github.com/osfans/trime/commit/61eee60cbe9e07d5f533346ccbebe2ecd4e08b64

--- script/cache-hash.sh
+++ script/cache-hash.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-JNI_FILES="magic.txt app/build.gradle app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
+MAGIC=magic.txt
+echo auto generated contents >> $MAGIC
+
+# fetch jni relative elements
+grep ndkVersion app/build.gradle >> $MAGIC
+
+elments=("buildTypes" "externalNativeBuild" "splits")
+for element in ${elments[@]}; do
+  awk "/$element/,/\}/" app/build.gradle >> $MAGIC
+done
+
+JNI_FILES="$MAGIC app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
 
 hash=$(git submodule status)
 hash=$hash$(sha256sum $JNI_FILES)
