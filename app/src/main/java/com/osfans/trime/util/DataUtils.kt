7a6c25075e225a72c702c4e74bdf8bcbb1c6c0b8
tumuyan <tumuyan@gmail.com>
2021-12-26 13:18:55 +0800

    feat: backup and recover perf xml file when sync

--- app/src/main/java/com/osfans/trime/util/DataUtils.kt
+++ app/src/main/java/com/osfans/trime/util/DataUtils.kt
@@ -1,7 +1,15 @@
 package com.osfans.trime.util
 
+import com.osfans.trime.BuildConfig
+import com.osfans.trime.Rime
 import com.osfans.trime.ime.core.Preferences
+import timber.log.Timber
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.Exception
 
 object DataUtils {
     private val prefs get() = Preferences.defaultInstance()
@@ -20,5 +28,50 @@ object DataUtils {
         } else {
             File(userDataDir, childdir).path
         }
+    }
+
+    fun copyFile(file: File, outputPath: String): Boolean {
+        if (file.exists()) {
+            try {
+                val input: InputStream = FileInputStream(file)
+                val outputStream: OutputStream = FileOutputStream(File(outputPath))
+                val buffer = ByteArray(1024)
+                var len: Int
+                while (input.read(buffer).also { len = it } > 0) {
+                    outputStream.write(buffer, 0, len)
+                }
+                Timber.i("copyFile = ${file.absolutePath}, $outputPath")
+                return true
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return false
+    }
+
+    fun backupPref(): Boolean {
+        val file = File(
+            "/data/data/APPLICATION_ID/shared_prefs/APPLICATION_ID_preferences.xml"
+                .replace("APPLICATION_ID", BuildConfig.APPLICATION_ID)
+        )
+
+        return copyFile(
+            file,
+            Rime.get_sync_dir() + File.separator + Rime.get_user_id() + File.separator + file.name
+        )
+    }
+
+    fun recoverPref(): Boolean {
+        val file = File(Rime.get_sync_dir() + File.separator + Rime.get_user_id() + File.separator + "recover.xml")
+
+        if (copyFile(
+                file,
+                "/data/data/APPLICATION_ID/shared_prefs/APPLICATION_ID_preferences.xml"
+                    .replace("APPLICATION_ID", BuildConfig.APPLICATION_ID)
+            )
+        ) {
+            return file.renameTo(File(file.parent, "recovered.xml"))
+        }
+        return false
     }
 }
--- app/src/main/java/com/osfans/trime/util/RimeUtils.kt
+++ app/src/main/java/com/osfans/trime/util/RimeUtils.kt
@@ -28,6 +28,13 @@ object RimeUtils {
     /** Sync the user data.
      * @return `true` if successfully **/
     suspend fun sync(context: Context): Boolean = withContext(Dispatchers.IO) {
+
+        // 备份 /data/data/APPLICATION_ID/shared_prefs/APPLICATION_ID_preferences.xml 到同步目录
+        DataUtils.backupPref()
+        // 从同步目录恢复 revocer.xml 到data目录，并且重命名recover.xml为recovered.xml
+        DataUtils.recoverPref()
+        // TODO 自动刷新同文的设置（目前需要手动结束进程才能生效
+
         Rime.sync_user_data().also {
             Rime.destroy()
             Rime.get(context, true)
