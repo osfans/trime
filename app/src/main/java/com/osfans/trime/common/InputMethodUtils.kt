2bfa5d3a865ce9c5b9181d2db48400b0438c73bb
tumuyan <tumuyan@gmail.com>
2022-04-15 16:31:34 +0800

    perf: improve adaptation of setup activity

--- app/src/main/java/com/osfans/trime/common/InputMethodUtils.kt
+++ app/src/main/java/com/osfans/trime/common/InputMethodUtils.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Settings
 import android.view.inputmethod.InputMethodManager
+import com.osfans.trime.BuildConfig
 import com.osfans.trime.TrimeApplication
 import timber.log.Timber
 
 object InputMethodUtils {
-    private const val IME_ID: String = "com.osfans.trime/.TrimeImeService"
+
+    private const val IME_ID0: String = "com.osfans.trime/.TrimeImeService"
+    private const val IME_ID1: String = BuildConfig.APPLICATION_ID + "/com.osfans.trime.TrimeImeService"
+    //    var packageName = javaClass.getPackage().name
     private val context: Context
         get() = TrimeApplication.getInstance().applicationContext
 
@@ -17,8 +21,11 @@ object InputMethodUtils {
             context.contentResolver,
             Settings.Secure.ENABLED_INPUT_METHODS
         ) ?: "(none)"
+
         Timber.i("List of active IMEs: $activeImeIds")
-        return activeImeIds.split(":").contains(IME_ID)
+        if (activeImeIds.split(":").contains(IME_ID0))
+            return true
+        return activeImeIds.split(":").contains(IME_ID1)
     }
 
     fun checkIsTrimeSelected(): Boolean {
@@ -27,7 +34,9 @@ object InputMethodUtils {
             Settings.Secure.DEFAULT_INPUT_METHOD
         ) ?: "(none)"
         Timber.i("Selected IME: $selectedImeIds")
-        return selectedImeIds == IME_ID
+        if (selectedImeIds == IME_ID0)
+            return true
+        return selectedImeIds == IME_ID1
     }
 
     fun showImeEnablerActivity(context: Context) {
