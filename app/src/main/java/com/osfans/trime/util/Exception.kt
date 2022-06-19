39393f677e1a11acde38c2aa8b486b33871b6264
WhiredPlanck <whiredplanck@outlook.com>
2022-06-19 23:02:45 +0800

    refactor(util): new Logcat
    This enables application to catch log in runtime.
    
    - new Exception message template functions
    
    Ref: 1. https://github.com/fcitx5-android/fcitx5-android/blob/e44c1c7/app/src/main/java/org/fcitx/fcitx5/android/utils/Logcat.kt
    2. https://github.com/fcitx5-android/fcitx5-android/blob/e44c1c7/app/src/main/java/org/fcitx/fcitx5/android/utils/Exception.kt

---
+++ app/src/main/java/com/osfans/trime/util/Exception.kt
@@ -0,0 +1,28 @@
+/**
+ * Adapted from [fcitx5-android/Exception](https://github.com/fcitx5-android/fcitx5-android/blob/e44c1c7/app/src/main/java/org/fcitx/fcitx5/android/utils/Exception.kt)
+ */
+package com.osfans.trime.util
+
+import androidx.annotation.StringRes
+
+inline fun <T : Throwable> errorT(
+    cons: (String) -> T,
+    @StringRes messageTemplate: Int,
+    messageArg: String? = null
+): Nothing =
+    throw cons(
+        messageArg?.let {
+            appContext.getString(messageTemplate, it)
+        } ?: appContext.getString(
+            messageTemplate
+        )
+    )
+
+fun errorState(@StringRes messageTemplate: Int, messageArg: String? = null): Nothing =
+    errorT(::IllegalStateException, messageTemplate, messageArg)
+
+fun errorArg(@StringRes messageTemplate: Int, messageArg: String? = null): Nothing =
+    errorT(::IllegalArgumentException, messageTemplate, messageArg)
+
+fun errorRuntime(@StringRes messageTemplate: Int, messageArg: String? = null): Nothing =
+    errorT(::RuntimeException, messageTemplate, messageArg)
---
+++ app/src/main/java/com/osfans/trime/util/Logcat.kt
@@ -0,0 +1,88 @@
+/**
+ * Adapted from [fcitx5-android/Logcat.kt](https://github.com/fcitx5-android/fcitx5-android/blob/e44c1c7/app/src/main/java/org/fcitx/fcitx5/android/utils/Logcat.kt)
+ */
+package com.osfans.trime.util
+
+import android.os.Process
+import com.osfans.trime.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+
+class Logcat(val pid: Int? = Process.myPid()) : CoroutineScope by CoroutineScope(Dispatchers.IO) {
+
+    private var process: java.lang.Process? = null
+    private var emittingJob: Job? = null
+
+    private val flow: MutableSharedFlow<String> = MutableSharedFlow()
+
+    /**
+     * Subscribe to this flow to receive log in app
+     * Nothing would be emitted until [initLogFlow] was called
+     */
+    val logFlow: SharedFlow<String> by lazy { flow.asSharedFlow() }
+
+    /**
+     * Get a snapshot of logcat
+     */
+    fun getLogAsync(): Deferred<Result<List<String>>> = async {
+        runCatching {
+            Runtime.getRuntime()
+                .exec(arrayOf("logcat", pid?.let { "--pid=$it" } ?: "", "-d"))
+                .inputStream
+                .bufferedReader()
+                .readLines()
+        }
+    }
+
+    /**
+     * Clear logcat
+     */
+    fun clearLog(): Job =
+        launch {
+            runCatching { Runtime.getRuntime().exec(arrayOf("logcat", "-c")) }
+        }
+
+    /**
+     * Create a process reading logcat, sending lines to [logFlow]
+     */
+    fun initLogFlow() =
+        if (process != null)
+            errorState(R.string.exception_logcat_created)
+        else launch {
+            runCatching {
+                Runtime
+                    .getRuntime()
+                    .exec(arrayOf("logcat", pid?.let { "--pid=$it" } ?: "", "-v", "brief"))
+                    .also { process = it }
+                    .inputStream
+                    .bufferedReader()
+                    .lineSequence()
+                    .asFlow()
+                    .flowOn(Dispatchers.IO)
+                    .cancellable()
+                    .collect { flow.emit(it) }
+            }
+        }.also { emittingJob = it }
+
+    /**
+     * Destroy the reading process
+     */
+    fun shutdownLogFlow() {
+        process?.destroy()
+        emittingJob?.cancel()
+    }
+
+    companion object {
+        val default by lazy { Logcat() }
+    }
+}
