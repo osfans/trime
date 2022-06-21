305bf2b6bcfc81aff2cd909a65656350cc7911a7
WhiredPlanck <whiredplanck@outlook.com>
2022-06-21 22:46:39 +0800

    refactor(ui): new MainViewModel
    ViewModel in a lifecycle component that can help share necessary live data among Activities.

---
+++ app/src/main/java/com/osfans/trime/ui/main/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.osfans.trime.ui.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val toolbarTitle = MutableLiveData<String>()
+
+    val topOptionsMenu = MutableLiveData<Boolean>()
+
+    fun setToolbarTitle(title: String) {
+        toolbarTitle.value = title
+    }
+
+    fun enableTopOptionsMenu() {
+        topOptionsMenu.value = true
+    }
+
+    fun disableTopOptionsMenu() {
+        topOptionsMenu.value = false
+    }
+}
