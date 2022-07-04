0dfcf07fe3d701798a9f387d5a3c605f34f1ebfa
WhiredPlanck <whiredplanck@outlook.com>
2022-07-04 10:50:40 +0800

    fix(ui): fail to enable or disable schema(s) sometimes
    launch a build function when invoke the show function, this can avoid that the launch job inside the AlertDialog builder doesn't work. (But I don't figure out why)

--- app/src/main/java/com/osfans/trime/ui/components/SchemaPickerDialog.kt
+++ app/src/main/java/com/osfans/trime/ui/components/SchemaPickerDialog.kt
@@ -1,8 +1,5 @@
-@file:Suppress("DEPRECATION")
-
 package com.osfans.trime.ui.components
 
-import android.app.ProgressDialog
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
 import com.blankj.utilcode.util.ToastUtils
@@ -11,21 +8,20 @@ import com.osfans.trime.core.Rime
 import com.osfans.trime.util.ProgressBarDialogIndeterminate
 import com.osfans.trime.util.popup
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlinx.coroutines.Dispatchers
 
 class SchemaPickerDialog(
     private val context: Context
 ) : CoroutineScope by MainScope() {
 
-    private lateinit var availableSchemaList: List<Map<String, String?>>
     private lateinit var availableSchemaIds: Array<String?>
     private lateinit var checkedList: BooleanArray
 
     private suspend fun init() = withContext(Dispatchers.IO) {
-        availableSchemaList = Rime.get_available_schema_list() ?: listOf()
+        val availableSchemaList = Rime.get_available_schema_list() ?: listOf()
         val selectedSchemaList = Rime.get_selected_schema_list()
             ?: listOf<Map<String, String?>>()
 
@@ -38,6 +34,38 @@ class SchemaPickerDialog(
             .toBooleanArray()
     }
 
+    private fun buildAndShowDialog() {
+        val picker = AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+            .setTitle(R.string.pref_schemas)
+            .setNegativeButton(android.R.string.cancel, null)
+        if (availableSchemaIds.isEmpty()) {
+            picker.setMessage(R.string.no_schemas)
+        } else {
+            picker.setPositiveButton(android.R.string.ok) { _, _ ->
+                launch {
+                    val deploying =
+                        context.ProgressBarDialogIndeterminate(R.string.deploy_progress)
+                            .create()
+                    deploying.popup()
+                    val schemasToSelect = availableSchemaIds
+                        .filterIndexed { index, _ -> checkedList[index] }
+                        .toTypedArray()
+                    withContext(Dispatchers.IO) {
+                        Rime.select_schemas(schemasToSelect)
+                        Rime.destroy()
+                        Rime.get(context, true)
+                    }
+                    ToastUtils.showLong(R.string.deploy_finish)
+                    deploying.dismiss()
+                }
+            }
+                .setMultiChoiceItems(
+                    availableSchemaIds, checkedList
+                ) { _, id, isChecked -> checkedList[id] = isChecked }
+        }
+        picker.create().popup()
+    }
+
     /** 调用该方法显示对话框 **/
     fun show() = launch {
         val loading =
@@ -46,36 +74,6 @@ class SchemaPickerDialog(
         loading.popup()
         init()
         loading.dismiss()
-        val picker = AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
-            .setTitle(R.string.pref_schemas)
-            .setNegativeButton(android.R.string.cancel, null)
-        if (availableSchemaList.isEmpty()) {
-            picker.setMessage(R.string.no_schemas)
-        } else {
-            picker.setPositiveButton(android.R.string.ok) { _, _ ->
-                    val deploying =
-                        context.ProgressBarDialogIndeterminate(R.string.deploy_progress)
-                            .create()
-                    deploying.popup()
-                    val schemasToSelect = availableSchemaIds
-                        .filterIndexed { index, _ -> checkedList[index] }
-                        .toTypedArray()
-                    launch {
-                        withContext(Dispatchers.IO) {
-                            runCatching {
-                                Rime.select_schemas(schemasToSelect)
-                                Rime.destroy()
-                                Rime.get(context, true)
-                            }
-                        }
-                    }
-                    ToastUtils.showLong(R.string.deploy_finish)
-                    deploying.dismiss()
-                }
-                .setMultiChoiceItems(
-                    availableSchemaIds, checkedList
-                ) { _, id, isChecked -> checkedList[id] = isChecked }
-        }
-        picker.create().popup()
+        buildAndShowDialog()
     }
 }
