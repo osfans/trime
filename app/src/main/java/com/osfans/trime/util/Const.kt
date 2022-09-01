ca56a08c1379f1d7e09484c5e768e29a0486dcf3
WhiredPlanck <whiredplanck@outlook.com>
2022-09-01 22:54:46 +0800

    chore: polish methods to get custom build config fields
    - fine tune display version name format
    - implement inline clipboard service getter

--- app/src/main/java/com/osfans/trime/util/Const.kt
+++ app/src/main/java/com/osfans/trime/util/Const.kt
@@ -4,4 +4,7 @@ import com.osfans.trime.BuildConfig
 
 object Const {
     const val buildGitHash = BuildConfig.BUILD_GIT_HASH
+    const val displayVersionName = "${BuildConfig.BUILD_VERSION_NAME}-${BuildConfig.BUILD_TYPE}"
+    const val originalGitRepo = "https://github.com/osfans/trime"
+    const val currentGitRepo = BuildConfig.BUILD_GIT_REPO
 }
