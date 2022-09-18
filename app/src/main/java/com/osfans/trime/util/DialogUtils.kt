97cf4a00f48bbc4d93b5e34c7927c752911da6f2
WhiredPlanck <whiredplanck@outlook.com>
2022-09-18 18:08:45 +0800

    refactor(ui): remove old picker implementations

--- app/src/main/java/com/osfans/trime/util/DialogUtils.kt
+++ app/src/main/java/com/osfans/trime/util/DialogUtils.kt
@@ -69,22 +69,3 @@ fun LifecycleCoroutineScope.withLoadingDialog(
         }
     }
 }
-
-/**
- * Append layout params to a dialog and show it, which allow the dialog to show
- * outside of Activity of this application. This requires overlays permission.
- *
- * 追加布局参数到对话框并显示它，这可以让它在本应用的 Activity 之外显示。要求有悬浮窗权限。
- */
-fun Dialog.popup() {
-    this.window?.let { window ->
-        window.attributes.token = Trime.getServiceOrNull()?.window?.window?.decorView?.windowToken
-        window.attributes.type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-        } else {
-            WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG
-        }
-        window.addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM)
-    }
-    this.show()
-}
