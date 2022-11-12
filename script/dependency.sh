08749744725eac14f31976053ec06176b8fef9f9
WhiredPlanck <whiredplanck@outlook.com>
2022-11-12 23:15:58 +0800

    ci: not to require capnproto dependency anymore
    The upstream has dropped it already, and we also remove the submodule at 0afc710c.

--- script/dependency.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# build from submodule source, keep same version
-echo "$(nproc) threads to build"
-cd app/src/main/jni/capnproto/c++
-autoreconf -i
-./configure
-make -j$(nproc)
-sudo make install
