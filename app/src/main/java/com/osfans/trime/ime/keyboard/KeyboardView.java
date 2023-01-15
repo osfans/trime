b3ab8ab9ddeab53fd810fa473379109f23b3dae4
340dt7zw <122514954+340dt7zw@users.noreply.github.com>
2023-01-15 14:07:49 +0800

    fix: flicking screen when changing keyboard

--- app/src/main/java/com/osfans/trime/ime/keyboard/KeyboardView.java
+++ app/src/main/java/com/osfans/trime/ime/keyboard/KeyboardView.java
@@ -621,7 +621,6 @@ public class KeyboardView extends View implements View.OnClickListener {
     List<Key> keys = mKeyboard.getKeys();
     mKeys = keys.toArray(new Key[keys.size()]);
     setKeyboardBackground();
-    requestLayout();
     // Hint to reallocate the buffer if the size changed
     mKeyboardChanged = true;
     invalidateAllKeys();
