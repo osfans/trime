7d4cb36eafa2119a0e4da51c5ff431528a7b969a
WhiredPlanck <whiredplanck@outlook.com>
2023-02-21 14:55:39 +0800

    refactor,fix(data): enhance the exception handling when parsing schema file
    - Shrink the serializable data class to just keep necessary properties

--- app/src/main/java/com/osfans/trime/data/schema/RimeSchema.kt
+++ app/src/main/java/com/osfans/trime/data/schema/RimeSchema.kt
@@ -6,42 +6,30 @@ import kotlinx.serialization.Transient
 
 @Serializable
 data class RimeSchema(
-    @SerialName("__build_info")
-    val buildInfo: BuildInfo? = null, // 构建信息
     val schema: Schema, // 方案信息
     val switches: List<Switch> = listOf(), // 选项开关
     val punctuator: Punctuator? = null, // 标点
     val speller: Speller? = null // 拼写器
 ) {
     @Serializable
-    data class BuildInfo(
-        @SerialName("rime_version")
-        val rimeVersion: String,
-        val timestamps: Map<String, Long> = mapOf()
-    )
-
-    @Serializable
     data class Schema(
-        val author: List<String> = listOf(),
         @SerialName("schema_id")
         val schemaId: String,
-        val version: String? = null,
-        val dependencies: List<String> = listOf(),
-        val description: String? = null
+        val name: String? = null
     )
 
     @Serializable
     data class Switch(
         val name: String? = null,
-        val options: List<String> = listOf(),
+        val options: List<String>? = listOf(),
         val reset: Int? = null,
-        val states: List<String> = listOf(),
+        val states: List<String>? = listOf(),
         @Transient var enabled: Int = 0,
     )
 
     @Serializable
     data class Punctuator(
-        val symbols: Map<String, List<String>> = mapOf()
+        val symbols: Map<String, List<String>>? = mapOf()
     )
 
     @Serializable
--- app/src/main/java/com/osfans/trime/data/schema/SchemaManager.kt
+++ app/src/main/java/com/osfans/trime/data/schema/SchemaManager.kt
@@ -12,6 +12,8 @@ object SchemaManager {
     private lateinit var currentSchema: RimeSchema
     private lateinit var visibleSwitches: List<RimeSchema.Switch>
 
+    private val defaultSchema = RimeSchema(RimeSchema.Schema(".default"))
+
     private val arrow get() = AppPrefs.defaultInstance().keyboard.switchArrowEnabled
 
     private val yaml = Yaml(
@@ -22,16 +24,18 @@ object SchemaManager {
 
     @JvmStatic
     fun init(schemaId: String) {
-        val raw = File(DataManager.buildDir, "$schemaId.schema.yaml")
-            .inputStream()
-            .bufferedReader()
-            .readText()
-        currentSchema = yaml.decodeFromString(
-            RimeSchema.serializer(),
-            raw
-        )
+        val raw = runCatching {
+            File(DataManager.buildDir, "$schemaId.schema.yaml")
+                .bufferedReader().use { it.readText() }
+        }.getOrDefault("")
+        currentSchema = runCatching {
+            yaml.decodeFromString(
+                RimeSchema.serializer(),
+                raw
+            )
+        }.getOrDefault(defaultSchema)
         visibleSwitches = currentSchema.switches
-            .filter { it.states.isNotEmpty() } // 剔除没有 states 条目项的值，它们不作为开关使用
+            .filter { !it.states.isNullOrEmpty() } // 剔除没有 states 条目项的值，它们不作为开关使用
         updateSwitchOptions()
     }
 
@@ -42,7 +46,7 @@ object SchemaManager {
     fun updateSwitchOptions() {
         if (!this::visibleSwitches.isInitialized || visibleSwitches.isEmpty()) return // 無方案
         visibleSwitches.forEach { s ->
-            s.enabled = if (s.options.isEmpty()) { // 只有单 Rime 运行时选项的开关，开关名即选项名，标记其启用状态
+            s.enabled = if (s.options.isNullOrEmpty()) { // 只有单 Rime 运行时选项的开关，开关名即选项名，标记其启用状态
                 Rime.getRimeOption(s.name!!).compareTo(false)
             } else { // 带有一系列 Rime 运行时选项的开关，找到启用的选项并标记
                 // 将启用状态标记为此选项的索引值，方便切换时直接从选项列表中获取
@@ -56,17 +60,15 @@ object SchemaManager {
         if (!this::visibleSwitches.isInitialized || visibleSwitches.isEmpty()) return
         val switch = visibleSwitches[index]
         val enabled = switch.enabled
-        val next: Int
-        if (switch.options.isEmpty()) {
-            next = 1 - switch.enabled
-            Rime.setOption(switch.name!!, next == 1)
+        switch.enabled = if (switch.options.isNullOrEmpty()) {
+            (1 - enabled).also { next -> Rime.setOption(switch.name!!, next == 1) }
         } else {
             val options = switch.options
-            next = (enabled + 1) % options.size
-            Rime.setOption(options[enabled], false)
-            Rime.setOption(options[next], true)
+            ((enabled + 1) % options.size).also { next ->
+                Rime.setOption(options[enabled], false)
+                Rime.setOption(options[next], true)
+            }
         }
-        switch.enabled = next
     }
 
     @JvmStatic
@@ -75,8 +77,8 @@ object SchemaManager {
         return Array(visibleSwitches.size) {
             val switch = visibleSwitches[it]
             val enabled = switch.enabled
-            val text = switch.states[enabled]
-            val comment = if (switch.options.isEmpty()) {
+            val text = switch.states!![enabled]
+            val comment = if (switch.options.isNullOrEmpty()) {
                 "${if (arrow) "→ " else ""}${switch.states[1 - enabled]}"
             } else ""
             CandidateListItem(comment, text)
