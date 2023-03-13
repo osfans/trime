2c90d5a3ac6fe6909a67361aa0ca0ece6ac58993
WhiredPlanck <whiredplanck@outlook.com>
2023-03-13 23:43:26 +0800

    fix(data): enhance the exception handling when parsing schema file
    Also shrink the serializable data class to just keep necessary properties to avoid deserializing failure, until we find a good and elegant way to just read the parts we concerned from the schema.

--- app/src/main/java/com/osfans/trime/data/schema/RimeSchema.kt
+++ app/src/main/java/com/osfans/trime/data/schema/RimeSchema.kt
@@ -1,35 +1,14 @@
 package com.osfans.trime.data.schema
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
 @Serializable
 data class RimeSchema(
-    @SerialName("__build_info")
-    val buildInfo: BuildInfo? = null, // 构建信息
-    val schema: Schema, // 方案信息
     val switches: List<Switch> = listOf(), // 选项开关
     val punctuator: Punctuator? = null, // 标点
     val speller: Speller? = null, // 拼写器
 ) {
-    @Serializable
-    data class BuildInfo(
-        @SerialName("rime_version")
-        val rimeVersion: String,
-        val timestamps: Map<String, Long> = mapOf(),
-    )
-
-    @Serializable
-    data class Schema(
-        val author: List<String> = listOf(),
-        @SerialName("schema_id")
-        val schemaId: String,
-        val version: String? = null,
-        val dependencies: List<String> = listOf(),
-        val description: String? = null,
-    )
-
     @Serializable
     data class Switch(
         val name: String? = null,
