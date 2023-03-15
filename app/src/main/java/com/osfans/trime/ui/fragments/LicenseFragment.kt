eeb5e7df6ef0d2d6be8dc0ef56bdd8edac19f87c
WhiredPlanck <whiredplanck@outlook.com>
2023-03-16 02:00:33 +0800

    chore: migrate from cookpad/license-tools-plugin to mikepenz/AboutLibraries
    Ref: https://github.com/fcitx5-android/fcitx5-android/commit/9c8e234ce41ead46f5e24c450a5c2fe32db0dc65

--- app/src/main/java/com/osfans/trime/ui/fragments/LicenseFragment.kt
+++ app/src/main/java/com/osfans/trime/ui/fragments/LicenseFragment.kt
@@ -3,11 +3,14 @@ package com.osfans.trime.ui.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.Preference
+import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.entity.License
+import com.mikepenz.aboutlibraries.util.withJson
 import com.osfans.trime.R
-import com.osfans.trime.data.LibraryLicenseDao
 import com.osfans.trime.ui.components.PaddingPreferenceFragment
 import com.osfans.trime.ui.main.MainViewModel
 import kotlinx.coroutines.launch
@@ -18,24 +21,28 @@ class LicenseFragment : PaddingPreferenceFragment() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         lifecycleScope.launch {
-            LibraryLicenseDao.getAll().onSuccess { licenses ->
-                val context = preferenceManager.context
-                val screen = preferenceManager.createPreferenceScreen(context)
-                licenses.forEach { license ->
+            val context = preferenceManager.context
+            val screen = preferenceManager.createPreferenceScreen(context)
+            Libs.Builder()
+                .withJson(context, R.raw.aboutlibraries)
+                .build()
+                .libraries
+                .sortedBy {
+                    if (it.tag == "native") it.uniqueId.uppercase() else it.uniqueId.lowercase()
+                }
+                .forEach {
                     screen.addPreference(
                         Preference(context).apply {
                             isIconSpaceReserved = false
-                            title = license.libraryName
-                            summary = license.artifactId.group
-                            setOnPreferenceClickListener {
-                                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(license.licenseUrl)))
-                                true
+                            title = "${it.uniqueId}:${it.artifactVersion}"
+                            summary = it.licenses.joinToString { l -> l.spdxId ?: l.name }
+                            setOnPreferenceClickListener { _ ->
+                                showLicenseDialog(it.uniqueId, it.licenses)
                             }
                         },
                     )
                 }
-                preferenceScreen = screen
-            }
+            preferenceScreen = screen
         }
     }
 
@@ -43,5 +50,30 @@ class LicenseFragment : PaddingPreferenceFragment() {
         super.onResume()
         viewModel.setToolbarTitle(getString(R.string.about__license))
         viewModel.disableTopOptionsMenu()
+    }
+
+    private fun showLicenseDialog(uniqueId: String, licenses: Set<License>): Boolean {
+        when (licenses.size) {
+            0 -> {}
+            1 -> showLicenseContent(licenses.first())
+            else -> {
+                val licenseArray = licenses.toTypedArray()
+                val licenseNames = licenseArray.map { it.spdxId ?: it.name }.toTypedArray()
+                AlertDialog.Builder(requireContext())
+                    .setTitle(uniqueId)
+                    .setItems(licenseNames) { _, idx ->
+                        showLicenseContent(licenseArray[idx])
+                    }
+                    .setPositiveButton(android.R.string.cancel, null)
+                    .show()
+            }
+        }
+        return true
+    }
+
+    private fun showLicenseContent(license: License) {
+        if (license.url?.isNotBlank() == true) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(license.url)))
+        }
     }
 }
