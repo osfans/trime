c02e983f7cea50d15c13ebf10934ae56220f493b
WhiredPlanck <whiredplanck@outlook.com>
2023-03-16 13:12:33 +0800

    chore: set jdk version of kotlin for codegen module ...
    ... by kotlin.jvmToolchain option to keep jvm target compatibility.
    
    Ref: https://kotl.in/gradle/jvm/toolchain

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -8,6 +8,10 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+kotlin {
+    jvmToolchain(8)
+}
+
 dependencies {
     implementation("com.squareup:kotlinpoet-ksp:1.12.0")
     implementation("com.google.devtools.ksp:symbol-processing-api:1.8.0-1.0.8")
