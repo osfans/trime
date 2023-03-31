f96d2fa894e85228dcc01a64c40231bc6a78cba9
wxyzh <wxyzhen@live.com>
2023-03-31 12:12:38 +0800

    chore(CI):Deprecated "set-output" command; (script/cache-hash.sh)Modify filename to build.gradle.kts

--- script/cache-hash.sh
+++ script/cache-hash.sh
@@ -5,11 +5,11 @@ MAGIC=magic.txt
 echo auto generated contents >> $MAGIC
 
 # fetch jni relative elements
-grep ndkVersion app/build.gradle >> $MAGIC
+grep ndkVersion app/build.gradle.kts >> $MAGIC
 
 elments=("buildTypes" "externalNativeBuild" "splits")
 for element in ${elments[@]}; do
-  awk "/$element/,/\}/" app/build.gradle >> $MAGIC
+  awk "/$element/,/\}/" app/build.gradle.kts >> $MAGIC
 done
 
 JNI_FILES="$MAGIC app/src/main/jni/cmake/* app/src/main/jni/librime_jni/* app/src/main/jni/CMakeLists.txt"
@@ -18,4 +18,4 @@ hash=$(git submodule status)
 hash=$hash$($DIGEST_ALGORITHM $JNI_FILES)
 hash=$(echo $hash | $DIGEST_ALGORITHM | cut -c-64)
 
-echo "::set-output name=hash::$hash"
+echo "{hash}={hash}" >> $GITHUB_OUTPUT
