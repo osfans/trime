a09b6aad4371ddbd518ceff2b224165360b293a8
wxyzh <wxyzhen@live.com>
2023-06-02 12:39:18 +0800

    Build codegen use jdk 17

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 dependencies {
