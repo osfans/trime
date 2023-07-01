cd9883346213ab02f8d5c3efad2f73252170e8d1
Bambooin <ooverflow@outlook.com>
2023-07-01 10:53:49 +0800

    chore: bump version to 3.2.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId  = "com.osfans.trime"
         minSdk = 21
         targetSdk = 33
-        versionCode = 20230701
-        versionName = "3.2.13"
+        versionCode = 20230901
+        versionName = "3.2.14"
 
         multiDexEnabled = true
         setProperty("archivesBaseName", "trime-$versionName")
