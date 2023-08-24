e6ed88041df4b1ad488a23099b76c28639553305
shitlime <fwshitlime@outlook.com>
2023-08-24 11:58:17 +0800

    fix: refresh liquid keyboard's candidates view
    Fix the issue of liquid keyboard candidate view not updating.

--- app/src/main/java/com/osfans/trime/ime/symbol/LiquidKeyboard.kt
+++ app/src/main/java/com/osfans/trime/ime/symbol/LiquidKeyboard.kt
@@ -269,6 +269,7 @@ class LiquidKeyboard(private val context: Context) : ClipboardHelper.OnClipboard
                 TextInputManager.getInstance().onCandidatePressed(position)
                 if (Rime.isComposing) {
                     updateCandidates(Rime.candidatesWithoutSwitch.toList())
+                    notifyDataSetChanged()
                     keyboardView.scrollToPosition(0)
                 } else {
                     service.selectLiquidKeyboard(-1)
