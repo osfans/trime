774bffd8fdbf566429fd3e90b3efd6834a96b10b
WhiredPlanck <whiredplanck@outlook.com>
2023-12-12 20:38:43 +0800

    refactor: fix compile error due to dependency update

--- app/src/main/java/com/osfans/trime/ime/lifecycle/LifecycleInputMethodService.kt
+++ app/src/main/java/com/osfans/trime/ime/lifecycle/LifecycleInputMethodService.kt
@@ -1,39 +1,25 @@
 package com.osfans.trime.ime.lifecycle
 
 import android.inputmethodservice.InputMethodService
-import android.view.View
 import androidx.annotation.CallSuper
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
-import androidx.lifecycle.ViewTreeLifecycleOwner
-import androidx.lifecycle.coroutineScope
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.setViewTreeLifecycleOwner
 
 open class LifecycleInputMethodService :
     InputMethodService(),
     LifecycleOwner {
     private val lifecycleRegistry by lazy { LifecycleRegistry(this) }
 
-    val uiScope: CoroutineScope
-        get() = lifecycle.coroutineScope
-
-    final override fun getLifecycle(): Lifecycle {
-        return lifecycleRegistry
-    }
+    override val lifecycle = lifecycleRegistry
 
     @CallSuper
     override fun onCreate() {
         super.onCreate()
+        window.window!!.decorView.setViewTreeLifecycleOwner(this)
         lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
         lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
-    }
-
-    @CallSuper
-    override fun onCreateInputView(): View? {
-        val decorView = window!!.window!!.decorView
-        ViewTreeLifecycleOwner.set(decorView, this)
-        return null
     }
 
     @CallSuper
