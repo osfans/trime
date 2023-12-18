78b519fae089bbd1122f054f5c4fbc0e385ddd3c
Goofyz Leung <rwcleung@gmail.com>
2023-12-18 10:19:27 +0800

    refactor: make `SimpleAdapter` faster
    Add multiple views to a horizontal `LinearLayout` and make it
    as a view for one item in `RecyclerView`, so that total number
    of items in `RecyclerView` can be reduced and scrolling can
    be faster.
    
    Refs #940

--- app/src/main/java/com/osfans/trime/ime/symbol/SimpleAdapter.java
+++ app/src/main/java/com/osfans/trime/ime/symbol/SimpleAdapter.java
@@ -8,14 +8,17 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.osfans.trime.R;
 import com.osfans.trime.data.theme.FontManager;
 import com.osfans.trime.data.theme.Theme;
-import com.osfans.trime.databinding.SimpleKeyItemBinding;
+import com.osfans.trime.databinding.SimpleItemOneBinding;
+import com.osfans.trime.databinding.SimpleItemRowBinding;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
   private final @NonNull List<SimpleKeyBean> mBeans = new ArrayList<>();
+  private final @NonNull List<List<SimpleKeyBean>> mBeansByRows = new ArrayList<>();
 
   @NonNull
   public final List<SimpleKeyBean> getBeans() {
@@ -23,62 +26,119 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder
   }
 
   private final Theme theme;
+  private final int columnSize;
 
-  public SimpleAdapter(@NonNull Theme theme) {
+  public SimpleAdapter(@NonNull Theme theme, int columnSize) {
     this.theme = theme;
+    this.columnSize = columnSize;
   }
 
   public void updateBeans(List<SimpleKeyBean> beans) {
     mBeans.clear();
     mBeans.addAll(beans);
+    mBeansByRows.clear();
+
+    ArrayList<SimpleKeyBean> t = new ArrayList<>();
+    for (int i = 0; i < mBeans.size(); i++) {
+      t.add(mBeans.get(i));
+      if ((i + 1) % columnSize == 0) {
+        mBeansByRows.add(t);
+        t = new ArrayList<>();
+      }
+    }
+    if (!t.isEmpty()) {
+      mBeansByRows.add(t);
+    }
   }
 
   @Override
   public int getItemCount() {
-    return mBeans.size();
+    return mBeansByRows.size();
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return position * 1000L;
   }
 
   @NonNull
   @Override
   public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    final SimpleKeyItemBinding binding =
-        SimpleKeyItemBinding.inflate(LayoutInflater.from(parent.getContext()));
-    binding.simpleKeyPin.setVisibility(View.INVISIBLE);
-    return new ViewHolder(binding);
+    final SimpleItemRowBinding binding =
+        SimpleItemRowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+    int size = parent.getResources().getDimensionPixelSize(R.dimen.simple_item_size);
+    ViewGroup.LayoutParams p = new ViewGroup.LayoutParams(size, size);
+
+    List<SimpleItemOneBinding> bindings = new ArrayList<>();
+    for (int i = 0; i < columnSize; i++) {
+      SimpleItemOneBinding view =
+          SimpleItemOneBinding.inflate(LayoutInflater.from(parent.getContext()), null, false);
+      bindings.add(view);
+      binding.wrapper.addView(view.getRoot(), p);
+    }
+    ViewHolder holder = new ViewHolder(binding, bindings);
+
+    for (int i = 0; i < holder.simpleKeyTexts.size(); i++) {
+      holder.wrappers.get(i).setTag((Integer) i);
+
+      TextView textView = holder.simpleKeyTexts.get(i);
+
+      textView.setTextColor(theme.colors.getColor("key_text_color"));
+      textView.setTypeface(FontManager.getTypeface(theme.style.getString("key_font")));
+      textView.setGravity(Gravity.CENTER);
+      textView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+
+      final float labelTextSize = theme.style.getFloat("label_text_size");
+      if (labelTextSize > 0) textView.setTextSize(labelTextSize);
+
+      textView.setBackground(
+          theme.colors.getDrawable(
+              "key_back_color", "key_border", "key_border_color", "round_corner", null));
+    }
+    return holder;
   }
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
-    final TextView simpleKeyText;
+    List<TextView> simpleKeyTexts;
+    List<ViewGroup> wrappers;
 
-    public ViewHolder(@NonNull SimpleKeyItemBinding binding) {
+    public ViewHolder(@NonNull SimpleItemRowBinding binding, List<SimpleItemOneBinding> views) {
       super(binding.getRoot());
 
-      simpleKeyText = binding.simpleKey;
+      simpleKeyTexts = new ArrayList<>();
+      wrappers = new ArrayList<>();
+      for (int i = 0; i < views.size(); i++) {
+        simpleKeyTexts.add((TextView) (views.get(i).getRoot().getChildAt(0)));
+        ((ViewGroup) views.get(i).getRoot()).getChildAt(1).setVisibility(View.GONE);
+        wrappers.add(views.get(i).getRoot());
+      }
     }
   }
 
   @Override
   public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-    final SimpleKeyBean bean = mBeans.get(position);
+    final List<SimpleKeyBean> bean = mBeansByRows.get(position);
 
-    holder.simpleKeyText.setText(bean.getLabel());
-    holder.simpleKeyText.setTextColor(theme.colors.getColor("key_text_color"));
-    holder.simpleKeyText.setTypeface(FontManager.getTypeface(theme.style.getString("key_font")));
-    holder.simpleKeyText.setGravity(Gravity.CENTER);
-    holder.simpleKeyText.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+    for (int i = 0; i < holder.simpleKeyTexts.size(); i++) {
+      holder.simpleKeyTexts.get(i).setText("");
+      if (i < bean.size()) {
+        holder.wrappers.get(i).setVisibility(View.VISIBLE);
+        holder.simpleKeyTexts.get(i).setText(bean.get(i).getLabel());
+      } else {
+        holder.wrappers.get(i).setVisibility(View.INVISIBLE);
+      }
 
-    final float labelTextSize = theme.style.getFloat("label_text_size");
-    if (labelTextSize > 0) holder.simpleKeyText.setTextSize(labelTextSize);
-
-    holder.itemView.setBackground(
-        theme.colors.getDrawable(
-            "key_back_color", "key_border", "key_border_color", "round_corner", null));
-
-    if (listener != null) {
-      holder.itemView.setOnClickListener(
-          view -> {
-            listener.onSimpleKeyClick(position);
-          });
+      if (listener != null) {
+        holder
+            .wrappers
+            .get(i)
+            .setOnClickListener(
+                view -> {
+                  if (view.getTag() != null) {
+                    listener.onSimpleKeyClick(position * columnSize + (int) view.getTag());
+                  }
+                });
+      }
     }
   }
 
