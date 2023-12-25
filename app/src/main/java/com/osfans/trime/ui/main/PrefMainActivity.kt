65182fa737b51b059f9e09abb9c3b867fa6e6230
Goofyz Leung <rwcleung@gmail.com>
2023-12-25 15:20:27 +0800

    refactor: set `RimeWrapper.canStart` if permissions granted

--- app/src/main/java/com/osfans/trime/ui/main/PrefMainActivity.kt
+++ app/src/main/java/com/osfans/trime/ui/main/PrefMainActivity.kt
@@ -181,6 +181,7 @@ class PrefMainActivity : AppCompatActivity() {
                     ) {
                         if (all) {
                             ToastUtils.showShort(R.string.external_storage_permission_granted)
+                            RimeWrapper.canStart = true
                             SoundThemeManager.init()
                         }
                     }
@@ -189,6 +190,7 @@ class PrefMainActivity : AppCompatActivity() {
                         permissions: List<String>,
                         never: Boolean,
                     ) {
+                        RimeWrapper.canStart = false
                         if (never) {
                             ToastUtils.showShort(R.string.external_storage_permission_denied)
                             XXPermissions.startPermissionActivity(
