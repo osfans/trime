c9fcf5a56c0003bf3ccb51e6fc8ffef8d5a93494
shitlime <fwshitlime@outlook.com>
2024-01-17 23:31:53 +0800

    build: support Java 21 compile

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -2,15 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlin {
-    jvmToolchain(17)
-}
-
 dependencies {
     implementation(libs.kotlinpoet.ksp)
     implementation(libs.ksp)
