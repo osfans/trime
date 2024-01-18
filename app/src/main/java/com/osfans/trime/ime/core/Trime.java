osfans/trime

commit db63dab1f7b0bb83e634da51dc50247d89597c51

fix: only call `updateComposing()` if not using `LiquidKeyboard` so tab will not scroll back to the start

app/src/main/java/com/osfans/trime/ime/core/Trime.java
@@ -441,8 +441,8 @@ public class Trime extends LifecycleInputMethodService {
       // 设置液体键盘处于隐藏状态
       TabManager.get().setTabExited();
       symbolInput.setVisibility(View.GONE);
+      updateComposing();
     }
-    updateComposing();
     mainInput.setVisibility(tabIndex >= 0 ? View.GONE : View.VISIBLE);
   }
 

commit 04e5a8397792acc6e2a47bcc41cc84d4329c95b7

fix: show toolbar whenever `selectLiquidKeyboard()` is called
This fixes an unintentional side effect caused by commit db63dab1f7b0bb83e634da51dc50247d89597c51.

refs #1177

app/src/main/java/com/osfans/trime/ime/core/Trime.java
@@ -330,6 +330,7 @@ public class Trime extends LifecycleInputMethodService {
         mTabRoot.setBackground(mCandidateRoot.getBackground());
         mTabRoot.move(tabView.getHightlightLeft(), tabView.getHightlightRight());
       }
+      showLiquidKeyboardToolbar();
     } else {
       symbolKeyboardType = SymbolKeyboardType.NO_KEY;
       // 设置液体键盘处于隐藏状态
@@ -1109,8 +1110,7 @@ public class Trime extends LifecycleInputMethodService {
         Timber.d("updateComposing() SymbolKeyboardType=%s", symbolKeyboardType.toString());
         if (symbolKeyboardType != SymbolKeyboardType.NO_KEY
             && symbolKeyboardType != SymbolKeyboardType.CANDIDATE) {
-          mComposition.setWindow();
-          showCompositionView(false);
+          showLiquidKeyboardToolbar();
         } else {
           mComposition.setVisibility(View.VISIBLE);
           startNum = mComposition.setWindow(minPopupSize, minPopupCheckSize, Integer.MAX_VALUE);
@@ -1131,6 +1131,11 @@ public class Trime extends LifecycleInputMethodService {
       setCandidatesViewShown(textInputManager.isComposable()); // 實體鍵盤打字時顯示候選欄
 
     return startNum;
+  }
+
+  private void showLiquidKeyboardToolbar() {
+    mComposition.changeToLiquidKeyboardToolbar();
+    showCompositionView(false);
   }
 
   public void showDialogAboveInputView(@NonNull final Dialog dialog) {

app/src/main/java/com/osfans/trime/ime/text/Composition.java
@@ -581,7 +581,7 @@ public class Composition extends AppCompatTextView {
   }
 
   /** 设置悬浮窗, 用于liquidKeyboard的悬浮窗工具栏 */
-  public void setWindow() {
+  public void changeToLiquidKeyboardToolbar() {
     if (getVisibility() != View.VISIBLE) return;
     if (liquid_keyboard_window_comp.isEmpty()) {
       this.setVisibility(GONE);