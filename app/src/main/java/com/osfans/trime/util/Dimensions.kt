363b83a0c4e55ade6c4bb6f2970711a4049a611c
WhiredPlanck <whiredplanck@outlook.com>
2024-02-16 11:02:42 +0800

    refactor: update APIs of the custom dimensions util

--- app/src/main/java/com/osfans/trime/util/Dimensions.kt
+++ app/src/main/java/com/osfans/trime/util/Dimensions.kt
@@ -2,7 +2,10 @@
 
 package com.osfans.trime.util
 
+import android.content.Context
 import android.content.res.Resources
+import android.util.TypedValue
+import android.view.View
 
 /**
  * Value of dp to value of px.
@@ -35,3 +38,15 @@ inline fun sp2px(value: Float): Float = value * Resources.getSystem().displayMet
  * @return value of px
  */
 inline fun sp2px(value: Int): Int = (value * Resources.getSystem().displayMetrics.scaledDensity).toInt()
+
+/** Converts Scaled Pixels to pixels. Returns an `Int` or a `Float` based on [value]'s type. */
+inline fun Context.sp(value: Int) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value.toFloat(), resources.displayMetrics).toInt()
+
+/** Converts Scaled Pixels to pixels. Returns an `Int` or a `Float` based on [value]'s type. */
+inline fun Context.sp(value: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, resources.displayMetrics)
+
+/** Converts Scaled Pixels to pixels. Returns an `Int` or a `Float` based on [value]'s type. */
+inline fun View.sp(value: Int) = context.sp(value)
+
+/** Converts Scaled Pixels to pixels. Returns an `Int` or a `Float` based on [value]'s type. */
+inline fun View.sp(value: Float) = context.sp(value)
