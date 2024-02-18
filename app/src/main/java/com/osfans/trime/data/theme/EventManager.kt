c5ff32480d901210a9ff36f7ef4c4311e0936f9e
nopdan <xci@live.com>
2024-02-18 21:39:54 +0800

    fix: space key label set as a scheme name

--- app/src/main/java/com/osfans/trime/data/theme/EventManager.kt
+++ app/src/main/java/com/osfans/trime/data/theme/EventManager.kt
@@ -1,17 +1,18 @@
 package com.osfans.trime.data.theme
 
+import android.view.KeyEvent
 import com.osfans.trime.ime.keyboard.Event
 
 object EventManager {
     private var eventCache = mutableMapOf<String, Event>()
 
-    @JvmStatic
     fun getEvent(eventId: String): Event {
         if (eventCache.containsKey(eventId)) {
             return eventCache[eventId]!!
         }
         val event = Event(eventId)
-        eventCache[eventId] = event
+        // 空格的 label 需要根据方案动态显示，所以不加入缓存
+        if (event.code != KeyEvent.KEYCODE_SPACE) eventCache[eventId] = event
         return event
     }
 
