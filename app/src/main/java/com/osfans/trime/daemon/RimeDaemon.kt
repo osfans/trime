8b4ef4843ac2242d110f99b63963395fc54ead71
WhiredPlanck <whiredplanck@outlook.com>
2024-04-12 10:29:55 +0800

    feat: show notification when restarting rime
    This may prevent the rime backend from killing by the system.

--- app/src/main/java/com/osfans/trime/daemon/RimeDaemon.kt
+++ app/src/main/java/com/osfans/trime/daemon/RimeDaemon.kt
@@ -1,10 +1,15 @@
 package com.osfans.trime.daemon
 
+import androidx.core.app.NotificationCompat
+import com.blankj.utilcode.util.NotificationUtils
+import com.osfans.trime.R
 import com.osfans.trime.core.Rime
 import com.osfans.trime.core.RimeApi
 import com.osfans.trime.core.RimeLifecycle
 import com.osfans.trime.core.whenReady
+import com.osfans.trime.util.appContext
 import kotlinx.coroutines.runBlocking
+import splitties.systemservices.notificationManager
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
@@ -70,12 +75,28 @@ object RimeDaemon {
             }
         }
 
+    private const val CHANNEL_ID = "rime-daemon"
+    private var restartId = 0
+
     /**
      * Restart Rime instance to deploy while keep the session
      */
     fun restartRime(fullCheck: Boolean = false) =
         lock.withLock {
+            val id = restartId++
+            NotificationUtils.notify(CHANNEL_ID, id) {
+                it.setSmallIcon(R.drawable.ic_baseline_sync_24)
+                    .setContentTitle(appContext.getString(R.string.rime_daemon))
+                    .setContentText(appContext.getString(R.string.restarting_rime))
+                    .setOngoing(true)
+                    .setProgress(100, 0, true)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+            }
             realRime.finalize()
             realRime.startup(fullCheck)
+            // cancel notification on ready
+            realRime.lifecycle.whenReady {
+                notificationManager.cancel(CHANNEL_ID, id)
+            }
         }
 }
