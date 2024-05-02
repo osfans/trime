fb18278367335dffa8d051f1c07c1326f019164a
WhiredPlanck <whiredplanck@outlook.com>
2024-05-02 19:00:26 +0800

    fix: crash on showing toast in LogActivity on some custom ROMs
    ToastUtils of AndroidUtilCode may cause this issue, so I adapt the toast utils from fcitx5-android.
    
    Ref: https://github.com/fcitx5-android/fcitx5-android/blob/59558c5b624359455911082b10750f4dcbd10fe8/app/src/main/java/org/fcitx/fcitx5/android/utils/Toast.kt

--- app/src/main/java/com/osfans/trime/ui/main/LogActivity.kt
+++ app/src/main/java/com/osfans/trime/ui/main/LogActivity.kt
@@ -55,7 +55,7 @@ class LogActivity : AppCompatActivity() {
                                 it.write(logView.currentLog)
                             }
                         }
-                    }?.toast()
+                    }?.let { toast(it) }
                 }
             }
     }
--- app/src/main/java/com/osfans/trime/util/Utils.kt
+++ app/src/main/java/com/osfans/trime/util/Utils.kt
@@ -24,8 +24,6 @@ import com.hjq.permissions.Permission
 import com.hjq.permissions.XXPermissions
 import com.osfans.trime.R
 import com.osfans.trime.TrimeApplication
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import splitties.experimental.InternalSplittiesApi
 import splitties.resources.withResolvedThemeAttribute
 import java.io.Serializable
@@ -33,33 +31,8 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
 
 val appContext: Context get() = TrimeApplication.getInstance().applicationContext
-
-@OptIn(ExperimentalContracts::class)
-inline fun <T : Any, U> Result<T?>.bindOnNotNull(block: (T) -> Result<U>): Result<U>? {
-    contract {
-        callsInPlace(block, InvocationKind.AT_MOST_ONCE)
-    }
-    return when {
-        isSuccess && getOrThrow() != null -> block(getOrThrow()!!)
-        isSuccess && getOrThrow() == null -> null
-        else -> Result.failure(exceptionOrNull()!!)
-    }
-}
-
-suspend fun <T> Result<T>.toast() =
-    withContext(Dispatchers.Main.immediate) {
-        onSuccess {
-            ToastUtils.showShort(R.string.setup__done)
-        }
-        onFailure {
-            ToastUtils.showShort(it.message)
-        }
-    }
 
 @OptIn(InternalSplittiesApi::class)
 fun Context.styledFloat(
