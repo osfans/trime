9166278b3136e93b8b0285343292f4fc7ef38781
WhiredPlanck <whiredplanck@outlook.com>
2024-05-08 05:08:20 +0800

    fix: work with the old synced asset files hierarchy

--- app/src/main/java/com/osfans/trime/util/ResourceUtils.kt
+++ app/src/main/java/com/osfans/trime/util/ResourceUtils.kt
@@ -10,9 +10,10 @@ object ResourceUtils {
     fun copyFile(
         filename: String,
         dest: File,
+        removedPrefix: String = "",
     ) = runCatching {
         appContext.assets.open(filename).use { i ->
-            File(dest, filename)
+            File(dest, filename.removePrefix(removedPrefix))
                 .also { it.parentFile?.mkdirs() }
                 .outputStream()
                 .use { o -> i.copyTo(o) }
