c8841ed9ac3d7b8a284ee46e98468e320c3d4708
WhiredPlanck <whiredplanck@outlook.com>
2025-02-04 14:30:42 +0800

    refactor: replace logcat DSL with `subprocess`

--- app/src/main/java/com/osfans/trime/util/Process.kt
+++ app/src/main/java/com/osfans/trime/util/Process.kt
@@ -10,6 +10,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
+
+fun subprocess(vararg commands: String): Process = Runtime.getRuntime().exec(commands)
 
 fun Process.asFlow(): Flow<String> =
     inputStream
@@ -18,3 +21,13 @@ fun Process.asFlow(): Flow<String> =
         .asFlow()
         .flowOn(Dispatchers.IO)
         .cancellable()
+
+suspend fun Process.readText() =
+    withContext(Dispatchers.IO) {
+        inputStream.bufferedReader().readText()
+    }
+
+suspend fun Process.readLines() =
+    withContext(Dispatchers.IO) {
+        inputStream.bufferedReader().readLines()
+    }
