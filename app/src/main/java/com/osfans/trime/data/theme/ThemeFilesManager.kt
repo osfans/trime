787e7f344917f1d20b454dc838841ce0bc172f73
WhiredPlanck <whiredplanck@outlook.com>
2025-03-04 18:56:15 +0800

    fix: couldn't load theme with over 200 anchors and aliases

--- app/src/main/java/com/osfans/trime/data/theme/ThemeFilesManager.kt
+++ app/src/main/java/com/osfans/trime/data/theme/ThemeFilesManager.kt
@@ -5,36 +5,18 @@
 
 package com.osfans.trime.data.theme
 
-import com.charleskorn.kaml.AnchorsAndAliases
-import com.charleskorn.kaml.Yaml
-import com.charleskorn.kaml.YamlConfiguration
 import timber.log.Timber
 import java.io.File
 
 object ThemeFilesManager {
-    private val yaml =
-        Yaml(
-            configuration =
-                YamlConfiguration(
-                    strictMode = false,
-                    anchorsAndAliases = AnchorsAndAliases.Permitted(200u),
-                ),
-        )
-
-    fun listThemes(dir: File): MutableList<ThemeStub> {
+    fun listThemes(dir: File): MutableList<Theme> {
         val files = dir.listFiles { _, name -> name.endsWith("trime.yaml") } ?: return mutableListOf()
         return files
             .sortedByDescending { it.lastModified() }
             .mapNotNull decode@{
                 val theme =
                     runCatching {
-                        yaml
-                            .decodeFromString(
-                                ThemeStub.serializer(),
-                                it.inputStream().bufferedReader().readText(),
-                            ).apply {
-                                configId = it.nameWithoutExtension
-                            }
+                        Theme(it.nameWithoutExtension, parseOnly = true)
                     }.getOrElse { e ->
                         Timber.w("Failed to decode theme file ${it.absolutePath}: ${e.message}")
                         return@decode null
