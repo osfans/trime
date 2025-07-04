564e69020485db7048c364ac854ae8e4e960f844
WhiredPlanck <whiredplanck@outlook.com>
2025-07-05 03:15:41 +0800

    chore: update ndk
    NDK 28.0.13004108
    CMake 3.31.6
    Support 16KB page size

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 object Versions {
-    const val DEFAULT_CMAKE = "3.22.1"
-    const val DEFAULT_NDK = "25.2.9519653"
+    const val DEFAULT_CMAKE = "3.31.6"
+    const val DEFAULT_NDK = "28.0.13004108"
 
     val supportedAbis = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
 }
