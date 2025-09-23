44bf05574d6df7dbdc6b220995fcfe44144acefa
WhiredPlanck <whiredplanck@outlook.com>
2025-09-24 00:27:09 +0800

    fix(SwitchOptionWindow): option switches might disappear after changing schema

--- app/src/main/java/com/osfans/trime/ime/option/SwitchOptionWindow.kt
+++ app/src/main/java/com/osfans/trime/ime/option/SwitchOptionWindow.kt
@@ -8,12 +8,11 @@ package com.osfans.trime.ime.option
 import android.content.Context
 import android.view.View
 import android.widget.PopupMenu
-import androidx.lifecycle.lifecycleScope
 import com.osfans.trime.R
 import com.osfans.trime.core.RimeMessage
+import com.osfans.trime.core.SchemaItem
 import com.osfans.trime.daemon.RimeSession
 import com.osfans.trime.daemon.launchOnReady
-import com.osfans.trime.data.schema.Schema
 import com.osfans.trime.data.schema.SchemaManager
 import com.osfans.trime.data.theme.Theme
 import com.osfans.trime.ime.bar.ui.ToolButton
@@ -21,7 +20,6 @@ import com.osfans.trime.ime.broadcast.InputBroadcastReceiver
 import com.osfans.trime.ime.core.TrimeInputMethodService
 import com.osfans.trime.ime.window.BoardWindow
 import com.osfans.trime.util.AppUtils
-import kotlinx.coroutines.launch
 import splitties.dimensions.dp
 import splitties.views.dsl.core.add
 import splitties.views.dsl.core.horizontalLayout
@@ -85,15 +83,20 @@ class SwitchOptionWindow(
         }
     }
 
-    private fun updateSchemaOptionEntries(switches: List<Schema.Switch>) {
-        adapter.submitList(switches.map { SwitchOptionEntry.fromSwitch(it) })
+    private fun updateSchemaOptionEntries() {
+        adapter.submitList(
+            SchemaManager.activeSchema.switches
+                .filter { it.states.size > 1 }
+                .map { SwitchOptionEntry.fromSwitch(it) },
+        )
+    }
+
+    override fun onRimeSchemaUpdated(schema: SchemaItem) {
+        updateSchemaOptionEntries()
     }
 
     override fun onRimeOptionUpdated(value: RimeMessage.OptionMessage.Data) {
-        val data =
-            SchemaManager.activeSchema.switches
-                .filter { it.states.size > 1 }
-        updateSchemaOptionEntries(data)
+        updateSchemaOptionEntries()
     }
 
     override fun onCreateView() = view
@@ -114,12 +117,7 @@ class SwitchOptionWindow(
     override fun onCreateBarView() = barExternalView
 
     override fun onAttached() {
-        val data =
-            SchemaManager.activeSchema.switches
-                .filter { it.states.size > 1 }
-        service.lifecycleScope.launch {
-            updateSchemaOptionEntries(data)
-        }
+        updateSchemaOptionEntries()
     }
 
     override fun onDetached() {
