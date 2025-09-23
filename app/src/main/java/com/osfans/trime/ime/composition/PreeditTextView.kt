4c51d44eebf9cd334ce4d02636af9aaa3293a0a4
WhiredPlanck <whiredplanck@outlook.com>
2025-09-23 11:43:22 +0800

    fix(PreeditTextView): avoid IndexOutOfBoundsException when moving cursor
    Set horizontal padding instead of start padding to expand touch event area.

--- app/src/main/java/com/osfans/trime/ime/composition/PreeditTextView.kt
+++ app/src/main/java/com/osfans/trime/ime/composition/PreeditTextView.kt
@@ -10,6 +10,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.TextView
+import androidx.core.math.MathUtils
 
 @SuppressLint("AppCompatCustomView")
 class PreeditTextView
@@ -19,29 +20,34 @@ constructor(
     attributeSet: AttributeSet? = null,
 ) : TextView(context, attributeSet) {
     var onMoveCursor: ((Int) -> Unit)? = null
-
-    private var touchX: Int = 0
-    private var newSel: CharSequence = ""
+    private var lastTapOffset = -1
+    private var newCursorPos = -1
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
-                val x = event.x - paddingLeft
-                val y = event.y - paddingTop
-                touchX = getOffsetForPosition(x, y)
-                newSel = text.subSequence(0, touchX).dropWhile { it.isWhitespace() }
+                val textString = text.toString()
+                lastTapOffset = MathUtils.clamp(
+                    getOffsetForPosition(x, y),
+                    0,
+                    textString.length,
+                )
+                val bytes = textString.substring(0, lastTapOffset).toByteArray()
+                newCursorPos = bytes.size
                 return true
             }
             MotionEvent.ACTION_UP -> {
-                onMoveCursor?.invoke(newSel.length)
-                touchX = 0
-                newSel = ""
+                onMoveCursor?.invoke(newCursorPos)
+                lastTapOffset = -1
+                newCursorPos = -1
                 return true
             }
             MotionEvent.ACTION_CANCEL -> {
-                touchX = 0
-                newSel = ""
+                lastTapOffset = -1
+                newCursorPos = -1
                 return true
             }
         }
