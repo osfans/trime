42a4e24605bc56c23f78ae88debb810d5f98b480
WhiredPlanck <whiredplanck@outlook.com>
2025-11-29 19:47:55 +0800

    fix(popup): potential crash on showing popup

--- app/src/main/java/com/osfans/trime/ime/popup/PopupAction.kt
+++ app/src/main/java/com/osfans/trime/ime/popup/PopupAction.kt
@@ -1,6 +1,6 @@
 /*
- * SPDX-License-Identifier: LGPL-2.1-or-later
- * SPDX-FileCopyrightText: Copyright 2021-2023 Fcitx5 for Android Contributors
+ * SPDX-FileCopyrightText: 2015 - 2025 Rime community
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 package com.osfans.trime.ime.popup
