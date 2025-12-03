4faccc9f7502510e5abdb5d697802bb77c563b86
WhiredPlanck <whiredplanck@outlook.com>
2025-12-04 04:04:54 +0800

    refactor(core): normalize key event forwarding from physical keyboard
    Make rime just care about Alt, Ctrl, Shift, CapsLock and Release modifier state when forwarding physical keyboard event, so we can cancel the rough filtering of NumLock in eb70fe4e2aeda1f2039164d459bf323712a53795.

--- app/src/main/java/com/osfans/trime/core/KeyValue.kt
+++ app/src/main/java/com/osfans/trime/core/KeyValue.kt
@@ -5,6 +5,7 @@
 
 package com.osfans.trime.core
 
+import android.view.KeyCharacterMap
 import android.view.KeyEvent
 
 @JvmInline
@@ -16,6 +17,21 @@ value class KeyValue(
     override fun toString() = "0x" + value.toString(16).padStart(4, '0')
 
     companion object {
-        fun fromKeyEvent(event: KeyEvent) = KeyValue(RimeKeyMapping.keyCodeToVal(event.keyCode))
+        fun fromKeyEvent(event: KeyEvent): KeyValue {
+            val charCode = event.unicodeChar
+            // try charCode first, allow upper and lower case characters generating different KeyValue
+            if (charCode != 0 &&
+                // skip \t, because it's charCode is different from KeyValue
+                charCode != '\t'.code &&
+                // skip \n, because rime wants \r for return
+                charCode != '\n'.code &&
+                // skip Android's private-use character
+                charCode != KeyCharacterMap.HEX_INPUT.code &&
+                charCode != KeyCharacterMap.PICKER_DIALOG_INPUT.code
+            ) {
+                return KeyValue(charCode)
+            }
+            return KeyValue(RimeKeyMapping.keyCodeToVal(event.keyCode))
+        }
     }
 }
