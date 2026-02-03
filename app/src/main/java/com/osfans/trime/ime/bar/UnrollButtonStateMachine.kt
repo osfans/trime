6334adaa9d9f51adf0c3b85f97ca2fbb5c1214bf
WhiredPlanck <whiredplanck@outlook.com>
2026-02-04 00:58:27 +0800

    refactor: simplify event transition of UnrollButtonStateMachine

--- app/src/main/java/com/osfans/trime/ime/bar/UnrollButtonStateMachine.kt
+++ app/src/main/java/com/osfans/trime/ime/bar/UnrollButtonStateMachine.kt
@@ -1,12 +1,12 @@
-// SPDX-FileCopyrightText: 2015 - 2024 Rime community
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * SPDX-FileCopyrightText: 2015 - 2026 Rime community
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 package com.osfans.trime.ime.bar
 
 import com.osfans.trime.ime.bar.UnrollButtonStateMachine.BooleanKey.UnrolledCandidatesEmpty
 import com.osfans.trime.ime.bar.UnrollButtonStateMachine.BooleanKey.UnrolledCandidatesHighlighted
-import com.osfans.trime.ime.bar.UnrollButtonStateMachine.State.AboutToAttachWindow
 import com.osfans.trime.ime.bar.UnrollButtonStateMachine.State.ClickToAttachWindow
 import com.osfans.trime.ime.bar.UnrollButtonStateMachine.State.ClickToDetachWindow
 import com.osfans.trime.ime.bar.UnrollButtonStateMachine.State.Hidden
@@ -16,7 +16,6 @@ import com.osfans.trime.util.TransitionBuildBlock
 
 object UnrollButtonStateMachine {
     enum class State {
-        AboutToAttachWindow,
         ClickToAttachWindow,
         ClickToDetachWindow,
         Hidden,
@@ -33,11 +32,10 @@ object UnrollButtonStateMachine {
         UnrolledCandidatesUpdated({
             from(Hidden) transitTo ClickToAttachWindow on (UnrolledCandidatesEmpty to false)
             from(ClickToAttachWindow) transitTo Hidden on (UnrolledCandidatesEmpty to true)
-            from(ClickToAttachWindow) transitTo AboutToAttachWindow on (UnrolledCandidatesHighlighted to true)
+            from(ClickToAttachWindow) transitTo ClickToDetachWindow on (UnrolledCandidatesHighlighted to true)
         }),
         UnrolledCandidatesAttached({
             from(ClickToAttachWindow) transitTo ClickToDetachWindow
-            from(AboutToAttachWindow) transitTo ClickToDetachWindow
         }),
         UnrolledCandidatesDetached({
             from(ClickToDetachWindow) transitTo Hidden on (UnrolledCandidatesEmpty to true)
