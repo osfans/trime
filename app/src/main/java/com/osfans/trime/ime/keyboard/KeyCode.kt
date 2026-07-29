d9d248ca018d1b1422a85722a9ba95247e5fbba2
WhiredPlanck <whiredplanck@outlook.com>
2026-07-29 13:51:52 +0800

    refactor: simplify KeyCode implementation

--- app/src/main/java/com/osfans/trime/ime/keyboard/KeyCode.kt
+++ app/src/main/java/com/osfans/trime/ime/keyboard/KeyCode.kt
@@ -6,501 +6,186 @@
 package com.osfans.trime.ime.keyboard
 
 import android.view.KeyEvent
+import com.osfans.trime.core.RimeKeyMapping
 import com.osfans.trime.util.virtualKeyCharacterMap
 import timber.log.Timber
-import java.util.EnumMap
 
-@Suppress("ktlint:standard:enum-entry-name-case")
-enum class KeyCode {
-    // 与原trime.yaml主题android_key/name小节相比，差异如下：
-    // 1. 数字开头的keyName添加了下划线(在init阶段已经修复)，受到影响的按键有： 0-12，3D_MODE
-    //
-    // 符号英文名称-图形对应关系可以参考 https://www.mianfeiziti.com/font_glyph-172795.htm
+object KeyCode {
+    private const val SYMBOL_CODE_OFFSET = 10000
+    private const val UPPER_CODE_OFFSET = 20000
 
-    VoidSymbol,
-    SOFT_LEFT,
-    SOFT_RIGHT,
-    HOME,
-    BACK,
-    CALL,
-    ENDCALL,
-    _0,
-    _1,
-    _2,
-    _3,
-    _4,
-    _5,
-    _6,
-    _7,
-    _8,
-    _9,
-    asterisk,
-    numbersign,
-    Up,
-    Down,
-    Left,
-    Right,
-    KP_Begin,
-    VOLUME_UP,
-    VOLUME_DOWN,
-    POWER,
-    CAMERA,
-    Clear,
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    k,
-    l,
-    m,
-    n,
-    o,
-    p,
-    q,
-    r,
-    s,
-    t,
-    u,
-    v,
-    w,
-    x,
-    y,
-    z,
-    comma,
-    period,
-    Alt_L,
-    Alt_R,
-    Shift_L,
-    Shift_R,
-    Tab,
-    space,
-    SYM,
-    EXPLORER,
-    ENVELOPE,
-    Return,
-    BackSpace,
-    grave,
-    minus,
-    equal,
-    bracketleft,
-    bracketright,
-    backslash,
-    semicolon,
-    apostrophe,
-    slash,
-    at,
-    NUM,
-    HEADSETHOOK,
-    FOCUS,
-    plus,
-    Menu,
-    NOTIFICATION,
-    Find,
-    MEDIA_PLAY_PAUSE,
-    MEDIA_STOP,
-    MEDIA_NEXT,
-    MEDIA_PREVIOUS,
-    MEDIA_REWIND,
-    MEDIA_FAST_FORWARD,
-    MUTE,
-    Page_Up,
-    Page_Down,
-    PICTSYMBOLS,
-    Mode_switch,
-    BUTTON_A,
-    BUTTON_B,
-    BUTTON_C,
-    BUTTON_X,
-    BUTTON_Y,
-    BUTTON_Z,
-    BUTTON_L1,
-    BUTTON_R1,
-    BUTTON_L2,
-    BUTTON_R2,
-    BUTTON_THUMBL,
-    BUTTON_THUMBR,
-    BUTTON_START,
-    BUTTON_SELECT,
-    BUTTON_MODE,
-    Escape,
-    Delete,
-    Control_L,
-    Control_R,
-    Caps_Lock,
-    Scroll_Lock,
-    Meta_L,
-    Meta_R,
-    function,
-    Sys_Req,
-    Pause,
-    Home,
-    End,
-    Insert,
-    Next,
-    MEDIA_PLAY,
-    MEDIA_PAUSE,
-    MEDIA_CLOSE,
-    MEDIA_EJECT,
-    MEDIA_RECORD,
-    F1,
-    F2,
-    F3,
-    F4,
-    F5,
-    F6,
-    F7,
-    F8,
-    F9,
-    F10,
-    F11,
-    F12,
-    Num_Lock,
-    KP_0,
-    KP_1,
-    KP_2,
-    KP_3,
-    KP_4,
-    KP_5,
-    KP_6,
-    KP_7,
-    KP_8,
-    KP_9,
-    KP_Divide,
-    KP_Multiply,
-    KP_Subtract,
-    KP_Add,
-    KP_Decimal,
-    KP_Separator,
-    KP_Enter,
-    KP_Equal,
-    parenleft,
-    parenright,
-    VOLUME_MUTE,
-    INFO,
-    CHANNEL_UP,
-    CHANNEL_DOWN,
-    ZOOM_IN,
-    ZOOM_OUT,
-    TV,
-    WINDOW,
-    GUIDE,
-    DVR,
-    BOOKMARK,
-    CAPTIONS,
-    SETTINGS,
-    TV_POWER,
-    TV_INPUT,
-    STB_POWER,
-    STB_INPUT,
-    AVR_POWER,
-    AVR_INPUT,
-    PROG_RED,
-    PROG_GREEN,
-    PROG_YELLOW,
-    PROG_BLUE,
-    APP_SWITCH,
-    BUTTON_1,
-    BUTTON_2,
-    BUTTON_3,
-    BUTTON_4,
-    BUTTON_5,
-    BUTTON_6,
-    BUTTON_7,
-    BUTTON_8,
-    BUTTON_9,
-    BUTTON_10,
-    BUTTON_11,
-    BUTTON_12,
-    BUTTON_13,
-    BUTTON_14,
-    BUTTON_15,
-    BUTTON_16,
-    LANGUAGE_SWITCH,
-    MANNER_MODE,
-    _3D_MODE,
-    CONTACTS,
-    CALENDAR,
-    MUSIC,
-    CALCULATOR,
-    Zenkaku_Hankaku,
-    Eisu_toggle,
-    Muhenkan,
-    Henkan,
-    Hiragana_Katakana,
-    yen,
-    RO,
-    Kana_Lock,
-    ASSIST,
-    BRIGHTNESS_DOWN,
-    BRIGHTNESS_UP,
-    MEDIA_AUDIO_TRACK,
-    SLEEP,
-    WAKEUP,
-    PAIRING,
-    MEDIA_TOP_MENU,
-    _11,
-    _12,
-    LAST_CHANNEL,
-    TV_DATA_SERVICE,
-    VOICE_ASSIST,
-    TV_RADIO_SERVICE,
-    TV_TELETEXT,
-    TV_NUMBER_ENTRY,
-    TV_TERRESTRIAL_ANALOG,
-    TV_TERRESTRIAL_DIGITAL,
-    TV_SATELLITE,
-    TV_SATELLITE_BS,
-    TV_SATELLITE_CS,
-    TV_SATELLITE_SERVICE,
-    TV_NETWORK,
-    TV_ANTENNA_CABLE,
-    TV_INPUT_HDMI_1,
-    TV_INPUT_HDMI_2,
-    TV_INPUT_HDMI_3,
-    TV_INPUT_HDMI_4,
-    TV_INPUT_COMPOSITE_1,
-    TV_INPUT_COMPOSITE_2,
-    TV_INPUT_COMPONENT_1,
-    TV_INPUT_COMPONENT_2,
-    TV_INPUT_VGA_1,
-    TV_AUDIO_DESCRIPTION,
-    TV_AUDIO_DESCRIPTION_MIX_UP,
-    TV_AUDIO_DESCRIPTION_MIX_DOWN,
-    TV_ZOOM_MODE,
-    TV_CONTENTS_MENU,
-    TV_MEDIA_CONTEXT_MENU,
-    TV_TIMER_PROGRAMMING,
-    Help,
-    NAVIGATE_PREVIOUS,
-    NAVIGATE_NEXT,
-    NAVIGATE_IN,
-    NAVIGATE_OUT,
-    STEM_PRIMARY,
-    STEM_1,
-    STEM_2,
-    STEM_3,
-    Pointer_UpLeft,
-    Pointer_DownLeft,
-    Pointer_UpRight,
-    Pointer_DownRight,
-    MEDIA_SKIP_FORWARD,
-    MEDIA_SKIP_BACKWARD,
-    MEDIA_STEP_FORWARD,
-    MEDIA_STEP_BACKWARD,
-    SOFT_SLEEP,
-    CUT,
-    COPY,
-    PASTE,
-    SYSTEM_NAVIGATION_UP,
-    SYSTEM_NAVIGATION_DOWN,
-    SYSTEM_NAVIGATION_LEFT,
-    SYSTEM_NAVIGATION_RIGHT,
-    ALL_APPS,
-    REFRESH,
-    THUMBS_UP,
-    THUMBS_DOWN,
-    PROFILE_SWITCH,
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H,
-    I,
-    J,
-    K,
-    L,
-    M,
-    N,
-    O,
-    P,
-    Q,
-    R,
-    S,
-    T,
-    U,
-    V,
-    W,
-    X,
-    Y,
-    Z,
-    exclam,
-    quotedbl,
-    dollar,
-    percent,
-    ampersand,
-    colon,
-    less,
-    greater,
-    question,
-    asciicircum,
-    underscore,
-    braceleft,
-    bar,
-    braceright,
-    asciitilde,
-    ;
+    private val SYMBOL_LABELS: Map<String, String> =
+        mapOf(
+            "exclam" to "!",
+            "quotedbl" to "\"",
+            "dollar" to "$",
+            "percent" to "%",
+            "ampersand" to "&",
+            "colon" to ":",
+            "less" to "<",
+            "greater" to ">",
+            "question" to "?",
+            "asciicircum" to "^",
+            "underscore" to "_",
+            "braceleft" to "{",
+            "bar" to "|",
+            "braceright" to "}",
+            "asciitilde" to "~",
+        )
 
-    companion object {
-        // librime keyname (x11) - trime keycode (兼容Android)
-        private val convertMap: HashMap<String, KeyCode> = hashMapOf()
-
-        // 部分符号的 trime keycode (兼容Android) - key label
-        private val reverseMap: EnumMap<KeyCode, String> = EnumMap(KeyCode::class.java)
-
-        init {
-            for (type in entries) {
-                convertMap[type.toString()] = type
-            }
-
-            // android keycode 包含的数字开头的按键
-            reverseMap[_3D_MODE] = "3D_MODE"
-            reverseMap[_0] = "0"
-            reverseMap[_1] = "1"
-            reverseMap[_2] = "2"
-            reverseMap[_3] = "3"
-            reverseMap[_4] = "4"
-            reverseMap[_5] = "5"
-            reverseMap[_6] = "6"
-            reverseMap[_7] = "7"
-            reverseMap[_8] = "8"
-            reverseMap[_9] = "9"
-            reverseMap[_8] = "8"
-            reverseMap[_9] = "9"
-
-            // android keycode 已包含的符号
-            reverseMap[grave] = "`"
-            reverseMap[at] = "@"
-            reverseMap[numbersign] = "#"
-            reverseMap[asterisk] = "*"
-            reverseMap[parenleft] = "("
-            reverseMap[parenright] = ")"
-            reverseMap[minus] = "-"
-            reverseMap[equal] = "="
-            reverseMap[plus] = "+"
-            reverseMap[bracketleft] = "["
-            reverseMap[bracketright] = "]"
-            reverseMap[backslash] = "\\"
-            reverseMap[semicolon] = ";"
-            reverseMap[apostrophe] = "'"
-            reverseMap[comma] = ","
-            reverseMap[period] = "."
-            reverseMap[slash] = "/"
-
-            // android keycode未包含的符号
-            reverseMap[exclam] = "!"
-            reverseMap[quotedbl] = "\""
-            reverseMap[dollar] = "$"
-            reverseMap[percent] = "%"
-            reverseMap[ampersand] = "&"
-            reverseMap[colon] = ":"
-            reverseMap[less] = "<"
-            reverseMap[greater] = ">"
-            reverseMap[question] = "?"
-            reverseMap[asciicircum] = "^"
-            reverseMap[underscore] = "_"
-            reverseMap[braceleft] = "{"
-            reverseMap[bar] = "|"
-            reverseMap[braceright] = "}"
-            reverseMap[asciitilde] = "~"
-
-            reverseMap.forEach {
-                convertMap[it.value] = it.key
-            }
-
-            // android keycode 包含的小键盘，仅用于输出label，不用于label转按键
-            reverseMap[KP_0] = "0"
-            reverseMap[KP_1] = "1"
-            reverseMap[KP_2] = "2"
-            reverseMap[KP_3] = "3"
-            reverseMap[KP_4] = "4"
-            reverseMap[KP_5] = "5"
-            reverseMap[KP_6] = "6"
-            reverseMap[KP_7] = "7"
-            reverseMap[KP_8] = "8"
-            reverseMap[KP_9] = "9"
-            reverseMap[KP_8] = "8"
-            reverseMap[KP_9] = "9"
+    private val SYMBOL_NAME_TO_CODE: Map<String, Int> =
+        SYMBOL_LABELS.keys.withIndex().associate { (i, name) ->
+            name to (SYMBOL_CODE_OFFSET + i)
         }
 
-        fun isStandardKey(code: Int) = (SOFT_LEFT.ordinal..KeyCode.PROFILE_SWITCH.ordinal).contains(code)
+    private val SYMBOL_CODE_TO_NAME: Map<Int, String> =
+        SYMBOL_NAME_TO_CODE.entries.associate { it.value to it.key }
 
-        private fun hasSymbolLabel(keycode: Int): Boolean {
-            if (keycode !in entries.indices) return false
-            return keycode >= A.ordinal || reverseMap.containsKey(entries[keycode])
+    private val UPPER_NAME_TO_CODE: Map<String, Int> =
+        listOf(
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+        ).withIndex().associate { (i, name) -> name to (UPPER_CODE_OFFSET + i) }
+
+    private val UPPER_CODE_TO_NAME: Map<Int, String> =
+        UPPER_NAME_TO_CODE.entries.associate { it.value to it.key }
+
+    private val CHAR_TO_CODE: Map<String, Int> =
+        SYMBOL_LABELS.entries.associate { (name, char) ->
+            char to (SYMBOL_NAME_TO_CODE[name] ?: KeyEvent.KEYCODE_UNKNOWN)
+        }.plus(
+            mapOf(
+                "," to KeyEvent.KEYCODE_COMMA,
+                "." to KeyEvent.KEYCODE_PERIOD,
+                "-" to KeyEvent.KEYCODE_MINUS,
+                "=" to KeyEvent.KEYCODE_EQUALS,
+                "/" to KeyEvent.KEYCODE_SLASH,
+                ";" to KeyEvent.KEYCODE_SEMICOLON,
+                "'" to KeyEvent.KEYCODE_APOSTROPHE,
+                "`" to KeyEvent.KEYCODE_GRAVE,
+                "[" to KeyEvent.KEYCODE_LEFT_BRACKET,
+                "]" to KeyEvent.KEYCODE_RIGHT_BRACKET,
+                "\\" to KeyEvent.KEYCODE_BACKSLASH,
+                "@" to KeyEvent.KEYCODE_AT,
+                "#" to KeyEvent.KEYCODE_POUND,
+                "*" to KeyEvent.KEYCODE_STAR,
+                "+" to KeyEvent.KEYCODE_PLUS,
+            ),
+        )
+
+    private fun rimeNameToCode(name: String): Int {
+        val rimeVal = RimeKeyMapping.nameToKeyVal(name)
+        if (rimeVal != RimeKeyMapping.RimeKey_VoidSymbol) {
+            val code = RimeKeyMapping.valToKeyCode(rimeVal)
+            if (code != KeyEvent.KEYCODE_UNKNOWN) return code
+        }
+        return KeyEvent.KEYCODE_UNKNOWN
+    }
+
+    private fun rimeCodeToName(code: Int): String? {
+        val rimeVal = RimeKeyMapping.keyCodeToVal(code)
+        if (rimeVal != RimeKeyMapping.RimeKey_VoidSymbol) {
+            val name = RimeKeyMapping.keyValToName(rimeVal)
+            if (name != "VoidSymbol") return name
+        }
+        return null
+    }
+
+    fun isStandardKey(code: Int): Boolean = code in 1 until SYMBOL_CODE_OFFSET
+
+    fun hasSymbolLabel(code: Int): Boolean = SYMBOL_CODE_TO_NAME.containsKey(code)
+
+    fun getSymbolLabel(code: Int): String {
+        val name = SYMBOL_CODE_TO_NAME[code] ?: return ""
+        return SYMBOL_LABELS[name] ?: ""
+    }
+
+    fun nameToKeyCode(name: String): Int {
+        Timber.d("nameToKeyCode: $name")
+        if (name.isEmpty()) return KeyEvent.KEYCODE_UNKNOWN
+
+        UPPER_NAME_TO_CODE[name]?.let { return it }
+        SYMBOL_NAME_TO_CODE[name]?.let { return it }
+        CHAR_TO_CODE[name]?.let { return it }
+
+        val androidCode = KeyEvent.keyCodeFromString("KEYCODE_$name")
+        if (androidCode > 0) return androidCode
+
+        val rimeCode = rimeNameToCode(name)
+        if (rimeCode != KeyEvent.KEYCODE_UNKNOWN) return rimeCode
+
+        return KeyEvent.KEYCODE_UNKNOWN
+    }
+
+    fun codeToKeyName(code: Int): String? {
+        UPPER_CODE_TO_NAME[code]?.let { return it }
+        SYMBOL_CODE_TO_NAME[code]?.let { return it }
+
+        val rimeName = rimeCodeToName(code)
+        if (rimeName != null) return rimeName
+
+        val androidName = KeyEvent.keyCodeToString(code)
+        if (androidName.isNotEmpty() && androidName != "KEYCODE_UNKNOWN") {
+            return androidName.removePrefix("KEYCODE_")
         }
 
-        fun getSymbolLabel(keycode: KeyCode): String = reverseMap[keycode] ?: ""
+        return null
+    }
 
-        fun getDisplayLabel(
-            keyCode: Int,
-            mask: Int,
-        ): String = if (isStandardKey(keyCode)) {
-            // Android keycode区域
-            if (virtualKeyCharacterMap.isPrintingKey(keyCode)) {
-                val charCode = virtualKeyCharacterMap.get(keyCode, mask)
-                Timber.d("getDisplayLabel: keyCode=$keyCode, mask=$mask, charCode=$charCode")
-                if (charCode > 0) {
-                    charCode.toChar().toString()
-                } else {
-                    virtualKeyCharacterMap.getDisplayLabel(keyCode).lowercase()
-                }
+    fun getDisplayLabel(
+        code: Int,
+        mask: Int,
+    ): String = if (isStandardKey(code)) {
+        if (virtualKeyCharacterMap.isPrintingKey(code)) {
+            val charCode = virtualKeyCharacterMap.get(code, mask)
+            Timber.d("getDisplayLabel: keyCode=$code, mask=$mask, charCode=$charCode")
+            if (charCode > 0) {
+                charCode.toChar().toString()
             } else {
-                codeToKeyName(keyCode)
+                virtualKeyCharacterMap.getDisplayLabel(code).lowercase()
             }
-        } else if (hasSymbolLabel(keyCode)) { // 可見符號
-            getSymbolLabel(entries.getOrNull(keyCode) ?: VoidSymbol)
         } else {
-            ""
+            val name = codeToKeyName(code) ?: ""
+            name.removePrefix("KP_")
         }
+    } else if (hasSymbolLabel(code)) {
+        getSymbolLabel(code)
+    } else {
+        codeToKeyName(code) ?: ""
+    }
 
-        fun codeToKeyName(ordinal: Int): String = (entries.getOrNull(ordinal) ?: VoidSymbol)
-            .name.substringAfter('_')
+    fun parse(repr: String): Pair<Int, Int> {
+        if (repr.isEmpty()) return 0 to 0
+        var modifiers = 0
+        var start = 0
+        while (true) {
+            val found = repr.indexOf('+', start)
+            if (found == -1) break
 
-        fun nameToKeyCode(name: String): KeyCode {
-            Timber.d("keycodeByName: $name")
-            return convertMap[name] ?: VoidSymbol
-        }
-
-        fun parse(repr: String): Pair<Int, Int> {
-            if (repr.isEmpty()) return 0 to 0
-            var modifiers = 0
-            var start = 0
-            while (true) {
-                val found = repr.indexOf('+', start)
-                if (found == -1) break
-
-                val token = repr.substring(start, found)
-                val modifier = when (token) {
+            val token = repr.substring(start, found)
+            val modifier =
+                when (token) {
                     "Shift" -> KeyEvent.META_SHIFT_ON
                     "Control" -> KeyEvent.META_CTRL_ON
                     "Alt" -> KeyEvent.META_ALT_ON
                     "Lock" -> KeyEvent.META_CAPS_LOCK_ON
                     else -> null
                 }
-                if (modifier != null) {
-                    modifiers = modifiers or modifier
-                } else {
-                    Timber.e("Unrecognized modifier '$token'")
-                    return 0 to 0
-                }
-                start = found + 1
-            }
-            val token = repr.substring(start)
-            val keycode = convertMap[token] ?: VoidSymbol
-            if (keycode == VoidSymbol) {
-                Timber.e("Unrecognized key '$token'")
+            if (modifier != null) {
+                modifiers = modifiers or modifier
+            } else {
+                Timber.e("Unrecognized modifier '$token'")
                 return 0 to 0
             }
-            return keycode.ordinal to modifiers
+            start = found + 1
         }
+        val token = repr.substring(start)
+        val keycode = nameToKeyCode(token)
+        if (keycode == 0) {
+            Timber.e("Unrecognized key '$token'")
+            return 0 to 0
+        }
+        return keycode to modifiers
     }
 }
