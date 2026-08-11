9c7779fc0d7633563e7360f40c9bdf93894f1444
ENOA-REIAH-UION <i@enoa.me>
2026-08-11 16:05:09 +0800

    refactor: cache space key actions
    Since commit 5b78b883, getLabel() dynamically resolves space key label.
    
    Caching KeyAction instances for space keys is now safe and no longer risks stale labl.

--- app/src/main/java/com/osfans/trime/data/theme/KeyActionManager.kt
+++ app/src/main/java/com/osfans/trime/data/theme/KeyActionManager.kt
@@ -4,7 +4,6 @@
 
 package com.osfans.trime.data.theme
 
-import android.view.KeyEvent
 import com.osfans.trime.data.theme.model.KeyActionToken
 import com.osfans.trime.ime.keyboard.KeyAction
 
@@ -13,13 +12,7 @@ object KeyActionManager {
 
     fun getAction(token: String) = getAction(KeyActionToken.Plain(token))
 
-    fun getAction(token: KeyActionToken): KeyAction = actionCache[token]
-        ?: KeyAction(token).also {
-            // 空格的 label 需要根据方案动态显示，所以不加入缓存
-            if (it.code != KeyEvent.KEYCODE_SPACE) {
-                actionCache[token] = it
-            }
-        }
+    fun getAction(token: KeyActionToken): KeyAction = actionCache.getOrPut(token) { KeyAction(token) }
 
     fun resetCache() = actionCache.clear()
 }
