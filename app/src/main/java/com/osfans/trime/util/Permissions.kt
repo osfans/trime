8d25f7a8dae9262ee472080a1228645817860dff
goofyz <rwcleung@gmail.com>
2026-08-25 15:33:35 +0800

    refactor: remove external storage permissions

--- app/src/main/java/com/osfans/trime/util/Permissions.kt
+++ app/src/main/java/com/osfans/trime/util/Permissions.kt
@@ -6,45 +6,7 @@
 package com.osfans.trime.util
 
 import android.content.Context
-import android.os.Environment
-import com.hjq.permissions.OnPermissionCallback
-import com.hjq.permissions.Permission
-import com.hjq.permissions.XXPermissions
-import com.osfans.trime.R
+import com.osfans.trime.data.sync.RimeDataSync
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun Context.isStorageAvailable(): Boolean = XXPermissions.isGranted(this, Permission.MANAGE_EXTERNAL_STORAGE) &&
-    Environment.getExternalStorageDirectory().absolutePath.isNotEmpty()
-
-fun Context.requestExternalStoragePermission() {
-    XXPermissions
-        .with(this)
-        .permission(Permission.MANAGE_EXTERNAL_STORAGE)
-        .request(
-            object : OnPermissionCallback {
-                override fun onGranted(
-                    permissions: List<String>,
-                    all: Boolean,
-                ) {
-                    if (all) {
-                        toast(R.string.external_storage_permission_granted)
-                    }
-                }
-
-                override fun onDenied(
-                    permissions: List<String>,
-                    never: Boolean,
-                ) {
-                    if (never) {
-                        toast(R.string.external_storage_permission_denied)
-                        XXPermissions.startPermissionActivity(
-                            this@requestExternalStoragePermission,
-                            permissions,
-                        )
-                    } else {
-                        toast(R.string.external_storage_permission_denied)
-                    }
-                }
-            },
-        )
-}
+inline fun Context.isStorageAvailable(): Boolean = RimeDataSync.isStorageAvailable(this)
